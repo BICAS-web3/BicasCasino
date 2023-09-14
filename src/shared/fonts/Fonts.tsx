e7ce4a7af48441d9ea01d13bf521203fcb851306
DoctorEenot <dreenot@gmail.com>
2023-09-14 11:28:38 +0300

    add header

--- src/shared/fonts/Fonts.tsx
+++ src/shared/fonts/Fonts.tsx
@@ -1,6 +1,41 @@
 import localFont from 'next/font/local';
 import { FC } from 'react';
+import { Source_Sans_3, Nunito_Sans } from 'next/font/google';
 
+const source_sans_3 = Source_Sans_3({
+    weight: ['200',
+        '300',
+        '400',
+        '500',
+        '600',
+        '700',
+        '800',
+        '900'],
+    subsets: ["cyrillic",
+        "cyrillic-ext",
+        "greek",
+        "greek-ext",
+        "latin",
+        "latin-ext",
+        "vietnamese"]
+});
+
+const nunito_sans = Nunito_Sans({
+    weight: ['200',
+        '300',
+        '400',
+        '500',
+        '600',
+        '700',
+        '800',
+        '900',
+        '1000'],
+    subsets: ["cyrillic",
+        "cyrillic-ext",
+        "latin",
+        "latin-ext",
+        "vietnamese"]
+});
 
 const DalekPinpointFont = localFont({
     src: [
@@ -40,6 +75,8 @@ export const Fonts: FC = () => (
             :root {
                 --font-dalek-pinpoint: ${DalekPinpointFont.style.fontFamily};
                 --font-neue-machina: ${NeueMachinaFont.style.fontFamily};
+                --font-source-sans-3: ${source_sans_3.style.fontFamily};
+                --font-nunito-sans: ${nunito_sans.style.fontFamily};
             }
         `}
     </style>
