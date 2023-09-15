ceba3b46bc1855df8098134359bdc259cd5e3f25
Anton <you@example.com>
2023-09-15 15:51:50 +0300

    is_odd - removed, wrap_link - added

--- src/widgets/LiveBets/LiveBets.tsx
+++ src/widgets/LiveBets/LiveBets.tsx
@@ -16,7 +16,6 @@ const LinkIcon: FC<{}> = p => {
 }
 
 interface LiveBetProps {
-    is_odd: boolean,
     trx_url: string,
     time: string,
     network_icon: string,
@@ -33,35 +32,37 @@ interface LiveBetProps {
 };
 const LiveBet: FC<LiveBetProps> = props => {
     return (
-        <div className={s.liveBets_list_item}>
-            <div className={s.liveBets_list_item_time_block}>
-                <span className={s.liveBets_list_item_time}>{props.time}</span>
-            </div>
-            <div className={s.liveBets_list_item_game_block}>
-                <img src={gameIco.src} alt="game-ico-preview"/>
-                <span className={s.liveBets_list_item_game}>{props.game_name}</span>
-            </div>
-            <div className={s.liveBets_list_item_player_block}>
-                <div className={s.liveBets_list_item_player_ico} style={{background: props.userBg}}>
-                    <span className={s.liveBets_list_item_player_ico_name}>B</span>
+        <a href={props.trx_url} target='_blank' className={s.liveBet_main_link}>
+            <div className={s.liveBets_list_item}>
+                <div className={s.liveBets_list_item_time_block}>
+                    <span className={s.liveBets_list_item_time}>{props.time}</span>
                 </div>
-                <span className={s.liveBets_list_item_player}>{props.player}</span>
+                <div className={s.liveBets_list_item_game_block}>
+                    <img src={gameIco.src} alt="game-ico-preview"/>
+                    <span className={s.liveBets_list_item_game}>{props.game_name}</span>
+                </div>
+                <div className={s.liveBets_list_item_player_block}>
+                    <div className={s.liveBets_list_item_player_ico} style={{background: props.userBg}}>
+                        <span className={s.liveBets_list_item_player_ico_name}>B</span>
+                    </div>
+                    <span className={s.liveBets_list_item_player}>{props.player}</span>
+                </div>
+                <div className={s.liveBets_list_item_address_block}>
+                    <span className={s.liveBets_list_item_address}>{props.gameAddress}</span>
+                </div>
+                <div className={s.liveBets_list_item_wager_block}>
+                    <img src={wagerIco.src} alt="wager-ico"/>
+                    <span className={s.liveBets_list_item_wager}>{props.wager}</span>
+                </div>
+                <div className={s.liveBets_list_item_multiplier_block}>
+                    <span className={s.liveBets_list_item_multiplier}>{props.multiplier}x</span>
+                </div>
+                <div className={s.liveBets_list_item_profit_block}>
+                    <span className={s.liveBets_list_item_profit}>+{props.profit}</span>
+                    <img src={wagerIco.src} alt="wager-ico"/>
+                </div>
             </div>
-            <div className={s.liveBets_list_item_address_block}>
-                <span className={s.liveBets_list_item_address}>{props.gameAddress}</span>
-            </div>
-            <div className={s.liveBets_list_item_wager_block}>
-                <img src={wagerIco.src} alt="wager-ico"/>
-                <span className={s.liveBets_list_item_wager}>{props.wager}</span>
-            </div>
-            <div className={s.liveBets_list_item_multiplier_block}>
-                <span className={s.liveBets_list_item_multiplier}>{props.multiplier}x</span>
-            </div>
-            <div className={s.liveBets_list_item_profit_block}>
-                <span className={s.liveBets_list_item_profit}>+{props.profit}</span>
-                <img src={wagerIco.src} alt="wager-ico"/>
-            </div>
-        </div>
+        </a>
     );
 }
 
