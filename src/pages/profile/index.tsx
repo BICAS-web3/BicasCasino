f45862e8347c72905036c7c68a677ba6b62f6105
Anton@habdevs <hramickih@yandex.ru>
2023-09-16 18:24:15 +0500

    feat: added recently games #24

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,7 +1,45 @@
 import {ProfileCard} from "@/widgets/ProfileCard";
+import {RecentlyPlayedGames} from "@/shared/ui/RecentlyPlayedGames";
+import coinflipImg from "@/public/media/recently_games/conflip.png";
+import dunkinImg from "@/public/media/recently_games/dunkin.png";
+import Head from "next/head";
+import {Layout} from "@/widgets/Layout";
+import s from "@/pages/styles.module.scss";
+import React from "react";
+import styles from './index.module.scss'
+const RecentlyGames = [
+  {
+    id: 1,
+    title: 'Coinflip',
+    text: 'A game where you have to beat\n' +
+      'your opponent with a chip',
+    imgBackground: coinflipImg
+  },
+  {
+    id: 1,
+    title: 'Dunkin caps',
+    text: 'A game where you have to beat\n' +
+      'your opponent with a chip',
+    imgBackground: dunkinImg
+  }
+]
 
 export default function Profile() {
   return (
-    <ProfileCard/>
+    <>
+      <Head>
+        <title>NFT Play | Account page</title>
+      </Head>
+
+      <Layout>
+        <div className={styles.containerCard}>
+          <ProfileCard/>
+          <div style={{margin: '20px'}}></div>
+          <RecentlyPlayedGames RecentlyGames={RecentlyGames}/>
+        </div>
+        <div style={{margin: '20px'}}></div>
+        {/*<RecentlyPlayedGames RecentlyGames={RecentlyGames}/>*/}
+      </Layout>
+    </>
   )
 }
---
+++ src/shared/ui/RecentlyPlayedGames/ui/ui.tsx
@@ -0,0 +1,31 @@
+import {FC} from "react";
+import styles from './ui.module.scss'
+import Image, {StaticImageData} from "next/image";
+
+interface IRecentlyGames {
+  id: number | null
+  title: string
+  text: string | null
+  imgBackground: StaticImageData | string
+}
+
+export const RecentlyPlayedGames: FC<{ RecentlyGames: IRecentlyGames[] }> = ({RecentlyGames}) => {
+
+  return (
+    <div className={styles.recentlyPlayedContainer}>
+      <p className={styles.recentlyPlayedHeading}>recently played</p>
+
+      {RecentlyGames.map((game: IRecentlyGames) => (
+        // style={{ backgroundImage: `url(${game.imgBackground})` }}
+        <div key={game.id} className={styles.gameItem}>
+          <Image src={game.imgBackground} alt={game.title} className={styles.gameImage} layout={'fill'}
+                 objectFit={'cover'} />
+          <div className={styles.content}>
+            <h2 className={styles.gameTitle}>{game.title}</h2>
+            <p>{game.text}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
