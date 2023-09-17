03ecbc43c8d8cad0822484d0dcfb4c079a5de579
Anton@habdevs <hramickih@yandex.ru>
2023-09-17 16:37:19 +0500

    upd: styles recentled #24

--- src/shared/ui/RecentlyPlayedGames/ui/ui.tsx
+++ src/shared/ui/RecentlyPlayedGames/ui/ui.tsx
@@ -12,14 +12,14 @@ interface IRecentlyGames {
 export const RecentlyPlayedGames: FC<{ RecentlyGames: IRecentlyGames[] }> = ({RecentlyGames}) => {
 
   return (
-    <div className={styles.recentlyPlayedContainer}>
-      <p className={styles.recentlyPlayedHeading}>recently played</p>
+    <div className={styles.recently_played_container}>
+      <p className={styles.recently_played_heading}>recently played</p>
       {RecentlyGames.map((game: IRecentlyGames) => (
-        <div key={game.id} className={styles.gameItem}>
-          <Image src={game.imgBackground} alt={game.title} className={styles.gameImage} layout={'fill'}
+        <div key={game.id} className={styles.game_item}>
+          <Image src={game.imgBackground} alt={game.title} className={styles.game_image} layout={'fill'}
                  objectFit={'cover'} />
           <div className={styles.content}>
-            <h2 className={styles.gameTitle}>{game.title}</h2>
+            <h2 className={styles.game_title}>{game.title}</h2>
             <p>{game.text}</p>
           </div>
         </div>
