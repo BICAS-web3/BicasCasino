5daa121c7145f9d9bb2bdd225a729586faf07ec2
Anton@habdevs <hramickih@yandex.ru>
2023-09-18 13:43:59 +0500

    upd: ImageLeague | added border left #24

--- src/shared/ui/ImageLeague/ui/ui.tsx
+++ src/shared/ui/ImageLeague/ui/ui.tsx
@@ -6,11 +6,11 @@ import styles from './ui.module.scss'
 export const ImageLeague: FC<{}> = () => {
   return (
     <div className={styles.image}>
-      <div className={styles.imageWrapper}>
+      <div className={styles.image_wrapper}>
         <Image src={bronzeLeague} alt={'ImageLeague'} width={147} height={147}
                objectFit="cover"/>
       </div>
-      <div className={styles.blurBackground}></div>
+      <div className={styles.blur_background}></div>
       <span className={styles.title}>bronze league</span>
     </div>
   )
