7226956c4b4719ceab7cb2dfac2157206703cde2
DoctorEenot <dreenot@gmail.com>
2023-09-21 18:38:39 +0300

    fixed some issues + formating

--- src/widgets/Layout/Layout.tsx
+++ src/widgets/Layout/Layout.tsx
@@ -3,13 +3,13 @@ import s from './styles.module.scss';
 
 import { SideBar } from '@/widgets/SideBar';
 import { Header } from '../header';
-import { LayoutModel } from '.';
-import {Blur} from "@/widgets/Blur/Blur";
+//import { LayoutModel } from '.';
+import { Blur } from "@/widgets/Blur/Blur";
 import { useUnit } from 'effector-react';
 import { SettingsInit } from '../SettingsInit';
 import { WagmiConfig } from 'wagmi';
 import { web3 } from '@/entities/web3';
-import {Footer} from "@/widgets/Footer";
+import { Footer } from "@/widgets/Footer";
 
 
 interface LayoutProps {
@@ -17,31 +17,29 @@ interface LayoutProps {
 }
 export const Layout = ({ children, ...props }: LayoutProps) => {
     const [
-        BlurActive,
         wagmiConfig
     ] = useUnit([
-        LayoutModel.$BlurActive,
         web3.$WagmiConfig
     ]);
 
 
     return (
         <>
-        <SettingsInit />
-      {wagmiConfig != null ?
+            <SettingsInit />
+            {wagmiConfig != null ?
                 <WagmiConfig config={wagmiConfig}>
-        <div className={s.page_container}>
-            <Header />
-            <div className={s.side_bar_wrapper}>
-                <SideBar />
-            </div>
-            <Blur />
-            <main>
-                {children}
-            </main>
-            <Footer />
-        </div>
-        </WagmiConfig> : <></>}
-    </>
+                    <div className={s.page_container}>
+                        <Header />
+                        <div className={s.side_bar_wrapper}>
+                            <SideBar />
+                        </div>
+                        <Blur />
+                        <main>
+                            {children}
+                        </main>
+                        <Footer />
+                    </div>
+                </WagmiConfig> : <></>}
+        </>
     )
 }
