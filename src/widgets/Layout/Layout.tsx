424603ab5978777c829c0a7870de7dbf9c594251
Anton <you@example.com>
2023-09-25 12:15:05 +0300

    everything fixed

--- src/widgets/Layout/Layout.tsx
+++ src/widgets/Layout/Layout.tsx
@@ -1,44 +1,34 @@
-import {ReactNode, useEffect} from 'react';
-import s from './styles.module.scss';
+import { ReactNode, useEffect } from "react";
+import s from "./styles.module.scss";
 
-import { SideBar } from '@/widgets/SideBar';
-import { Header } from '../header';
-import { LayoutModel } from '.';
-import {Blur} from "@/widgets/Blur/Blur";
-import { useUnit } from 'effector-react';
-import { Account } from '@/widgets/Account';
-import {Footer} from "@/widgets/Footer";
-import * as SidebarM from '@/widgets/SideBar/model'
+import { SideBar } from "@/widgets/SideBar";
+import { Header } from "../header";
+import { Blur } from "@/widgets/Blur/Blur";
+import { useUnit } from "effector-react";
+import { Account } from "@/widgets/Account";
+import { Footer } from "@/widgets/Footer";
+import * as SidebarM from "@/widgets/SideBar/model";
 
 interface LayoutProps {
-    children?: ReactNode[]
+  children?: ReactNode[];
 }
 export const Layout = ({ children, ...props }: LayoutProps) => {
+  const [isOpen, close] = useUnit([SidebarM.$isOpen, SidebarM.Close]);
 
-    const [
-        isOpen,
-        close
-    ] = useUnit([
-        SidebarM.$isOpen,
-        SidebarM.Close
-    ])
+  useEffect(() => {
+    if (window.innerWidth <= 650) close();
+  }, []);
 
-    useEffect(() => {
-        if(window.innerWidth <= 650) close()
-    }, [])
+  return (
+    <div className={s.page_container}>
+      <Header />
+      <div className={`${s.side_bar_wrapper} ${isOpen && s.sideBar_opened}`}>
+        <SideBar />
+      </div>
 
-    return (
-        <div className={s.page_container}>
-            <Header />
-            <div className={`${s.side_bar_wrapper} ${isOpen && s.sideBar_opened}`}>
-                <SideBar />
-            </div>
-
-            <Blur />
-            <main className={s.main_area}>
-                {children}
-            </main>
-            <Footer />
-        </div>
-    )
-}
+      <Blur />
+      <main className={s.main_area}>{children}</main>
+      <Footer />
+    </div>
+  );
+};
