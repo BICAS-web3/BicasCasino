6906e75c098b8ee0bcd9a01aeb153dd98a2f1394
DoctorEenot <dreenot@gmail.com>
2023-09-27 11:40:08 +0300

    change path & add router

--- src/widgets/ProfileCard/ui/ui.tsx
+++ src/widgets/ProfileCard/ui/ui.tsx
@@ -1,20 +1,23 @@
-import {UserAvatar} from "@/shared/ui/UserAvatar/ui/ui";
-import {FC} from "react";
-import {UserName} from "@/shared/ui/UserName/ui/ui";
+import { UserAvatar } from "@/shared/ui/UserAvatar/ui/ui";
+import { FC } from "react";
+import { UserName } from "@/shared/ui/UserName/ui/ui";
 import emptyAvatar from '@/public/media/player_icons/emptyAvatar.svg'
-import {UserAddress} from "@/shared/ui/UserAddress/ui/ui";
+import { UserAddress } from "@/shared/ui/UserAddress/ui/ui";
 import styles from './ui.module.scss'
-import {ImageLeague} from "@/shared/ui/ImageLeague";
+import { ImageLeague } from "@/shared/ui/ImageLeague";
 // TODO: address - you can get it from @habdevs
 //  the store and see how it is used in the Profile component
 
-export const ProfileCard: FC<{}> = () => {
+export interface ProfileCardProps {
+  address: string
+};
+export const ProfileCard: FC<ProfileCardProps> = props => {
   return (
     <div className={styles.card}>
-      <UserAvatar avatarUrl={emptyAvatar}/>
+      <UserAvatar avatarUrl={emptyAvatar} />
       <div className={styles.user}>
-        <UserName userName={'Athena'}/>
-        <UserAddress address={'0xa51313ffab4c570b484a26bd5d869c2f48e34475'}/>
+        <UserName userName={'Athena'} />
+        <UserAddress address={props.address} />
       </div>
       <ImageLeague />
     </div>
