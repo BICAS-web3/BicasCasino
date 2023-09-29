622aaf5bc6bc11135a7ce9e3d371c966f8c52208
Anton <you@example.com>
2023-09-29 10:29:29 +0300

    poker-page - added

---
+++ src/pages/Poker/index.tsx
@@ -0,0 +1,10 @@
+import { GamePage } from "@/widgets/GamePage/GamePage";
+import { Poker } from "@/widgets/Poker/Poker";
+
+export default function PokerGame() {
+  return (
+    <GamePage>
+      <Poker />
+    </GamePage>
+  );
+}
