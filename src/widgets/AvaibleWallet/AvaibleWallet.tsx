ef1f5f74595acd95757517f335803ddc3b6d9294
Rishat2000 <risatosmonov@gmail.com>
2023-10-05 01:25:45 +0600

    added none-click to Conect Wallet

--- src/widgets/AvaibleWallet/AvaibleWallet.tsx
+++ src/widgets/AvaibleWallet/AvaibleWallet.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { FC } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import s from "./styles.module.scss";
 import Ledger from "@/public/media/select_wallet/Ledger.svg";
 import Coinbase from "@/public/media/select_wallet/Coinbase.svg";
@@ -10,54 +10,79 @@ import Close from "@/public/media/select_wallet/Close.svg";
 import { InfoIcon } from "@/shared/SVGs";
 
 export interface WalletProps {
-  icon: string;
-  name: string;
+	icon: string;
+	name: string;
 }
 const Wallet: FC<WalletProps> = (props) => {
-  return (
-    <div className={s.select_wallet_item}>
-      <Image src={props.icon} alt={""} />
-      <div className={s.text_icon}>{props.name}</div>
-    </div>
-  );
+	return (
+		<div className={s.select_wallet_item}>
+			<Image src={props.icon} alt={""} />
+			<div className={s.text_icon}>{props.name}</div>
+		</div>
+	);
 };
 
 export interface AvaibleWalletProps {
-  hideAvaibleWallet: () => void;
+	hideAvaibleWallet: () => void;
+	walletVisibility: boolean;
+	setWalletVisibility: (value: boolean) => void;
+	popupRef: React.RefObject<HTMLDivElement>;
+	buttonRef: React.RefObject<HTMLDivElement>
 }
+
 export const AvaibleWallet: FC<AvaibleWalletProps> = (props) => {
-  return (
-    <div className={s.avaibleWallet_container}>
-      <div className={s.avaibleWallet}>
-        <div className={s.main_text}>Available Wallet</div>
-        <button className={s.btn_close} onClick={props.hideAvaibleWallet}>
-          <Image
-            src={Close}
-            alt={""}
-            width={17}
-            height={17}
-            className={s.close_icon}
-          />
-        </button>
-      </div>
-      <div className={s.connect_text}>Connect Wallet</div>
-      <div className={s.select_wallet}>
-        <Wallet name="Ledger" icon={Ledger} />
-        <Wallet name="Trust Wallet" icon={Trust_wallet} />
-        <Wallet name="Coinbase" icon={Coinbase} />
-        <Wallet name="WalletConnect" icon={WalletConnect} />
-      </div>
-      <div className={s.info}>
-        <a
-          href="https://www.freecodecamp.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={s.text_info}
-        >
-          I don’t have a wallet
-        </a>
-        <InfoIcon />
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		const handleOutsideClick = (e: MouseEvent) => {
+			if (props.walletVisibility && e.target instanceof Node) {
+				if ((!props.popupRef.current || !props.popupRef.current.contains(e.target)) && (!props.buttonRef.current || !props.buttonRef.current.contains(e.target))) {
+					props.setWalletVisibility(true); // Изменяем состояние в родительском компоненте
+					props.hideAvaibleWallet();
+				}
+			}
+		};
+		if (props.walletVisibility) {
+			document.addEventListener('click', handleOutsideClick);
+		} else {
+			document.removeEventListener('click', handleOutsideClick);
+		}
+
+		return () => {
+			document.removeEventListener('click', handleOutsideClick);
+		};
+	}, [props.walletVisibility, props.setWalletVisibility]);
+
+	return (
+		<div className={s.avaibleWallet_container} ref={props.popupRef}>
+			<div className={s.avaibleWallet}>
+				<div className={s.main_text}>Available Wallet</div>
+				<button className={s.btn_close} onClick={props.hideAvaibleWallet}>
+					<Image
+						src={Close}
+						alt={""}
+						width={17}
+						height={17}
+						className={s.close_icon}
+					/>
+				</button>
+			</div>
+			<div className={s.connect_text}>Connect Wallet</div>
+			<div className={s.select_wallet}>
+				<Wallet name="Ledger" icon={Ledger} />
+				<Wallet name="Trust Wallet" icon={Trust_wallet} />
+				<Wallet name="Coinbase" icon={Coinbase} />
+				<Wallet name="WalletConnect" icon={WalletConnect} />
+			</div>
+			<div className={s.info}>
+				<a
+					href="https://www.freecodecamp.org/"
+					target="_blank"
+					rel="noopener noreferrer"
+					className={s.text_info}
+				>
+					I don’t have a wallet
+				</a>
+				<InfoIcon />
+			</div>
+		</div>
+	);
 };
