18f180cd0caefbd5b3566971ac037ada8e26f34f
Anton <you@example.com>
2023-10-04 14:41:34 +0300

    cardsHeight - fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -328,7 +328,9 @@ export default function Home() {
         <title>NFT Play | Home page</title>
       </Head>
 
-      <Layout>
+      <PokerGame />
+
+      {/* <Layout>
         <div className={s.background_container}>
           <Image src={MainPageBackground} alt={""} className={s.background} />
           <div className={s.background_gradient}></div>
@@ -362,7 +364,7 @@ export default function Home() {
           />
           <LeaderBoard />
         </div>
-      </Layout>
+      </Layout> */}
 
       {/* <Footer />
       <InvitesList />
