24bbba957aefaa807aa0c4b6be880c482d64392a
Anton <you@example.com>
2023-10-04 19:24:49 +0300

    index.tsx - cleared

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -328,9 +328,7 @@ export default function Home() {
         <title>NFT Play | Home page</title>
       </Head>
 
-      <CoinFlipGame />
-
-      {/* <Layout>
+      <Layout>
         <div className={s.background_container}>
           <Image src={MainPageBackground} alt={""} className={s.background} />
           <div className={s.background_gradient}></div>
@@ -364,7 +362,7 @@ export default function Home() {
           />
           <LeaderBoard />
         </div>
-      </Layout> */}
+      </Layout>
 
       {/* <Footer />
       <InvitesList />
