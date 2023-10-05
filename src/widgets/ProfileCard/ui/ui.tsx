6aa8e325c53b9904d7443beedcce186e02a25335
Anton@habdevs <hramickih@yandex.ru>
2023-10-05 14:37:43 +0500

    feat: interim commit on card layout â#104

--- src/widgets/ProfileCard/ui/ui.tsx
+++ src/widgets/ProfileCard/ui/ui.tsx
@@ -1,23 +1,36 @@
 import { UserAvatar } from "@/shared/ui/UserAvatar/ui/ui";
 import { FC } from "react";
 import { UserName } from "@/shared/ui/UserName/ui/ui";
-import emptyAvatar from '@/public/media/player_icons/emptyAvatar.svg'
+import emptyAvatar from "@/public/media/player_icons/emptyAvatar.svg";
 import { UserAddress } from "@/shared/ui/UserAddress/ui/ui";
-import styles from './ui.module.scss'
+import styles from "./ui.module.scss";
 import { ImageLeague } from "@/shared/ui/ImageLeague";
 
 export interface ProfileCardProps {
-  address: string
-};
-export const ProfileCard: FC<ProfileCardProps> = props => {
+  address: string;
+}
+export const ProfileCard: FC<ProfileCardProps> = (props) => {
   return (
-    <div className={styles.card}>
-      <UserAvatar avatarUrl={emptyAvatar} />
-      <div className={styles.user}>
-        <UserName userName={'Athena'} />
-        <UserAddress address={props.address} />
+    <div className={styles.user_wrapper}>
+      <div className={styles.card}>
+        <UserAvatar avatarUrl={emptyAvatar} />
+        <div className={styles.user}>
+          <UserName userName={"Athena"} />
+          <UserAddress address={props.address} />
+        </div>
       </div>
       <ImageLeague />
     </div>
-  )
-}
+  );
+};
+
+// return (
+//     <div className={styles.card}>
+//         <UserAvatar avatarUrl={emptyAvatar} />
+//         <div className={styles.user}>
+//             <UserName userName={'Athena'} />
+//             <UserAddress address={props.address} />
+//         </div>
+//         <ImageLeague />
+//     </div>
+// )
