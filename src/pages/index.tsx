7dafd3b9ca857b3bef826d612e5c59c70087cc51
Rishat2000 <risatosmonov@gmail.com>
2023-10-06 20:05:54 +0600

    fixed background image on main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,7 @@ import BSCNetworkIcon from "@/public/media/networks/bsc.svg";
 //import LinkIcon from '@/public/media/misc/link.svg';
 import { LiveBets } from "@/widgets/LiveBets";
 import MainPageBackground from "@/public/media/misc/MainPageBackground.png";
+import MainPageSecondBackground from "@/public/media/misc/MainPageSecondBackground.png";
 import { SideBar, SideBarModel } from "@/widgets/SideBar";
 
 import DiceBackground from "@/public/media/games_assets/dice/Background.png";
@@ -44,194 +45,194 @@ import CoinFlipGame from "./games/CoinFlip";
 const mobileQuery = "(max-width: 650px)";
 
 const LinkIcon: FC<{}> = (p) => {
-  return (
-    <svg height="14px" width="14px" viewBox="0 0 18 18">
-      <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M2 2V16H16V9H18V16C18 17.1 17.1 18 16 18H2C0.89 18 0 17.1 0 16V2C0 0.9 0.89 0 2 0H9V2H2Z"
-      ></path>
-      <path d="M11 0V2H14.59L4.76 11.83L6.17 13.24L16 3.41V7H18V0H11Z"></path>
-    </svg>
-  );
+	return (
+		<svg height="14px" width="14px" viewBox="0 0 18 18">
+			<path
+				fill-rule="evenodd"
+				clip-rule="evenodd"
+				d="M2 2V16H16V9H18V16C18 17.1 17.1 18 16 18H2C0.89 18 0 17.1 0 16V2C0 0.9 0.89 0 2 0H9V2H2Z"
+			></path>
+			<path d="M11 0V2H14.59L4.76 11.83L6.17 13.24L16 3.41V7H18V0H11Z"></path>
+		</svg>
+	);
 };
 
 interface GameProps {
-  name: string;
-  description: string;
-  link: string;
-  image: StaticImageData;
+	name: string;
+	description: string;
+	link: string;
+	image: StaticImageData;
 }
 
 const Game: FC<GameProps> = (props) => {
-  const [mobile, setMobile] = useState<any>(
-    //window.innerWidth <= 650 ? true : false,
-    false
-  );
+	const [mobile, setMobile] = useState<any>(
+		//window.innerWidth <= 650 ? true : false,
+		false
+	);
 
-  useEffect(() => {
-    setMobile(window.innerWidth <= 650 ? true : false);
-    let mediaQuery = window.matchMedia(mobileQuery);
-    mediaQuery.onchange = (e) => {
-      if (e.matches) {
-        setMobile(true);
-      } else {
-        setMobile(false);
-      }
-    };
-    return () => {
-      mediaQuery.onchange = null;
-    };
-  }, []);
-  //const [mobile] = useMatchMedia(mobileQuery);
+	useEffect(() => {
+		setMobile(window.innerWidth <= 650 ? true : false);
+		let mediaQuery = window.matchMedia(mobileQuery);
+		mediaQuery.onchange = (e) => {
+			if (e.matches) {
+				setMobile(true);
+			} else {
+				setMobile(false);
+			}
+		};
+		return () => {
+			mediaQuery.onchange = null;
+		};
+	}, []);
+	//const [mobile] = useMatchMedia(mobileQuery);
 
-  return (
-    <a
-      className={s.game_link}
-      href={props.link}
-      style={{
-        backgroundImage: `url(${
-          mobile ? props.imageMobile.src : props.image.src
-        })`,
-      }}
-    >
-      {/* <Image
+	return (
+		<a
+			className={s.game_link}
+			href={props.link}
+			style={{
+				backgroundImage: `url(
+					${mobile ? props.imageMobile.src : props.image.src
+					})`,
+			}}
+		>
+			{/* <Image
             src={DiceBackground}
             alt={''}
             className={s.game_image}
         /> */}
-      <div className={s.game}>
-        <div className={s.game_info}>
-          <div className={s.game_name}>
-            {props.name}
-            {/* <div className={s.game_arrow}>
+			< div className={s.game} >
+				<div className={s.game_info}>
+					<div className={s.game_name}>
+						{props.name}
+						{/* <div className={s.game_arrow}>
                         {'>'}
                     </div> */}
-          </div>
-          <div className={s.game_description}>{props.description}</div>
-        </div>
-      </div>
-    </a>
-  );
+					</div>
+					<div className={s.game_description}>{props.description}</div>
+				</div>
+			</div >
+		</a >
+	);
 };
 
 interface GameProps {
-  name: string;
-  description: string;
-  link: string;
-  image_colored: any;
-  image_blend: any;
-  imageMobile: any;
+	name: string;
+	description: string;
+	link: string;
+	image_colored: any;
+	image_blend: any;
+	imageMobile: any;
 }
 
 const GameBlured: FC<GameProps> = (props) => {
-  // const [isHovering, setIsHovered] = useState(false);
+	// const [isHovering, setIsHovered] = useState(false);
 
-  // const onMouseEnter = () => setIsHovered(true);
-  // const onMouseLeave = () => setIsHovered(false);
+	// const onMouseEnter = () => setIsHovered(true);
+	// const onMouseLeave = () => setIsHovered(false);
 
-  return (
-    <div className={s.game_link}>
-      <div className={s.game_blured}>
-        <div className={s.comming_soon}>
-          <div>Comming Soon</div>
-        </div>
-        <div className={`${s.game_info} ${s.blured}`}>
-          <div className={s.game_name}>
-            {props.name}
-            <div className={s.game_arrow}>{">"}</div>
-          </div>
-          <div className={s.game_description}>{props.description}</div>
-        </div>
+	return (
+		<div className={s.game_link}>
+			<div className={s.game_blured}>
+				<div className={s.comming_soon}>
+					<div>Comming Soon</div>
+				</div>
+				<div className={`${s.game_info} ${s.blured}`}>
+					<div className={s.game_name}>
+						{props.name}
+						<div className={s.game_arrow}>{">"}</div>
+					</div>
+					<div className={s.game_description}>{props.description}</div>
+				</div>
 
-        <Image
-          className={`${s.game_icon} ${s.blured}`}
-          src={props.image_colored}
-          alt=""
-          width={undefined}
-          height={undefined}
-        />
-      </div>
-    </div>
-  );
+				<Image
+					className={`${s.game_icon} ${s.blured}`}
+					src={props.image_colored}
+					alt=""
+					width={undefined}
+					height={undefined}
+				/>
+			</div>
+		</div>
+	);
 };
 
-interface GamesProps {}
+interface GamesProps { }
 
 const Games: FC<GamesProps> = (props) => {
-  return (
-    <div className={s.games}>
-      {/* <GamesTitle></GamesTitle> */}
-      <div className={s.games_row}>
-        <Game
-          name={"COINFLIP"}
-          description={
-            "COINFLIP GAME very long description that needs to be wrapped to the new line"
-          }
-          link={"/games/CoinFlip"}
-          image_colored={CoinFlipColoredIcon}
-          image_blend={CoinFlipBlendIcon}
-          image={CoinflipBackground}
-          imageMobile={pokerMobileBg}
-        />
-        <Game
-          name={"ROCK PAPER SCISSORS"}
-          description={
-            "COINFLIP GAME very long description that needs to be wrapped to the new line"
-          }
-          link={"/games/RockPaperScissors"}
-          image_colored={RPSColoredIcon}
-          image_blend={RPSBlendIcon}
-          image={DiceBackground}
-          imageMobile={rockPaperScissorsMobileBg}
-        />
-        {/* <Game
+	return (
+		<div className={s.games}>
+			{/* <GamesTitle></GamesTitle> */}
+			<div className={s.games_row}>
+				<Game
+					name={"COINFLIP"}
+					description={
+						"COINFLIP GAME very long description that needs to be wrapped to the new line"
+					}
+					link={"/games/CoinFlip"}
+					image_colored={CoinFlipColoredIcon}
+					image_blend={CoinFlipBlendIcon}
+					image={CoinflipBackground}
+					imageMobile={pokerMobileBg}
+				/>
+				<Game
+					name={"ROCK PAPER SCISSORS"}
+					description={
+						"COINFLIP GAME very long description that needs to be wrapped to the new line"
+					}
+					link={"/games/RockPaperScissors"}
+					image_colored={RPSColoredIcon}
+					image_blend={RPSBlendIcon}
+					image={DiceBackground}
+					imageMobile={rockPaperScissorsMobileBg}
+				/>
+				{/* <Game
                 name={'ROCK PAPER SCISSORS'}
                 description={'COINFLIP GAME very long description that needs to be wrapped to the new line'}
                 link={'/games/rock-paper-scissors'}
                 image_colored={RPSColoredIcon}
                 image_blend={RPSBlendIcon}
             /> */}
-      </div>
+			</div>
 
-      <div className={s.games_row}>
-        {/* <GameBlured
+			<div className={s.games_row}>
+				{/* <GameBlured
                 name={'DICE'}
                 description={'COINFLIP GAME very long description that needs to be wrapped to the new line'}
                 link={'/games/dice'}
                 image_colored={DiceColoredIcon}
                 image_blend={DiceBlendIcon}
             /> */}
-        <Game
-          name={"DICE"}
-          description={
-            "DICE GAME very long description that needs to be wrapped to the new line"
-          }
-          link={"/games/Dice"}
-          image_colored={DiceColoredIcon}
-          image_blend={DiceBlendIcon}
-          image={RPSBackground}
-          imageMobile={pokerMobileBg}
-        />
+				<Game
+					name={"DICE"}
+					description={
+						"DICE GAME very long description that needs to be wrapped to the new line"
+					}
+					link={"/games/Dice"}
+					image_colored={DiceColoredIcon}
+					image_blend={DiceBlendIcon}
+					image={RPSBackground}
+					imageMobile={pokerMobileBg}
+				/>
 
-        {/* <Game
+				{/* <Game
                 name={'COINFLIP'}
                 description={'COINFLIP GAME very long description that needs to be wrapped to the new line'}
                 link={'/games/CoinFlip'}
                 image_colored={CoinFlipColoredIcon}
                 image_blend={CoinFlipBlendIcon}
             />  */}
-      </div>
-    </div>
-  );
+			</div>
+		</div>
+	);
 };
 
-interface GamesTitleProps {}
+interface GamesTitleProps { }
 const GamesTitle: FC<GamesTitleProps> = (props) => {
-  return (
-    <div className={s.games_title}>
-      <div>Games</div>
-      {/* <div className={s.games_more}>
+	return (
+		<div className={s.games_title}>
+			<div>Games</div>
+			{/* <div className={s.games_more}>
             <div>
                 Show More
             </div>
@@ -239,8 +240,8 @@ const GamesTitle: FC<GamesTitleProps> = (props) => {
                 {'>'}
             </div>
         </div> */}
-    </div>
-  );
+		</div>
+	);
 };
 
 // interface TotalProps { name: string, value: string };
@@ -272,104 +273,104 @@ const GamesTitle: FC<GamesTitleProps> = (props) => {
 //     </div>)
 // }
 
-interface BannerInfoProps {}
+interface BannerInfoProps { }
 const BannerInfo: FC<BannerInfoProps> = (props) => {
-  const [isOpen, isMainWalletOpen, close, open, setBlur] = useUnit([
-    SideBarModel.$isOpen,
-    MainWallet.$isMainWalletOpen,
-    MainWallet.Close,
-    MainWallet.Open,
-    BlurModel.setBlur,
-  ]);
+	const [isOpen, isMainWalletOpen, close, open, setBlur] = useUnit([
+		SideBarModel.$isOpen,
+		MainWallet.$isMainWalletOpen,
+		MainWallet.Close,
+		MainWallet.Open,
+		BlurModel.setBlur,
+	]);
 
-  const hideAvaibleWallet = () => {
-    close();
-    setBlur(false);
-    document.documentElement.style.overflow = "visible";
-  };
+	const hideAvaibleWallet = () => {
+		close();
+		setBlur(false);
+		document.documentElement.style.overflow = "visible";
+	};
 
-  const handleConnectWalletBtn = () => {
-    if (!isMainWalletOpen) {
-      open();
-      setBlur(true);
-      document.documentElement.style.overflow = "hidden";
-      window.scrollTo(0, 0);
-    } else {
-      close();
-      setBlur(false);
-      document.documentElement.style.overflow = "visible";
-    }
-  };
+	const handleConnectWalletBtn = () => {
+		if (!isMainWalletOpen) {
+			open();
+			setBlur(true);
+			document.documentElement.style.overflow = "hidden";
+			window.scrollTo(0, 0);
+		} else {
+			close();
+			setBlur(false);
+			document.documentElement.style.overflow = "visible";
+		}
+	};
 
-  return (
-    <div className={s.banner_info}>
-      <div className={s.header}>Top 1 Casino on the WEB3</div>
-      <div className={s.connect_wallet_container}>
-        <div className={s.text}>Login via Web3 wallets</div>
-        <div className={s.button} onClick={handleConnectWalletBtn}>
-          Connect Wallet
-        </div>
-        <div
-          className={`${s.banner_info_avaibleWallet_container} ${
-            !isOpen && s.sidebarClosed
-          } ${isMainWalletOpen && s.walletVisible}`}
-        >
-          <AvaibleWallet hideAvaibleWallet={hideAvaibleWallet} />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={s.banner_info}>
+			<div className={s.header}>Top 1 Casino on the WEB3</div>
+			<div className={s.connect_wallet_container}>
+				<div className={s.text}>Login via Web3 wallets</div>
+				<div className={s.button} onClick={handleConnectWalletBtn}>
+					Connect Wallet
+				</div>
+				<div
+					className={`${s.banner_info_avaibleWallet_container} ${!isOpen && s.sidebarClosed
+						} ${isMainWalletOpen && s.walletVisible}`}
+				>
+					<AvaibleWallet hideAvaibleWallet={hideAvaibleWallet} />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>NFT Play | Home page</title>
-      </Head>
+	return (
+		<>
+			<Head>
+				<title>NFT Play | Home page</title>
+			</Head>
 
-      <Layout>
-        {/* <div> */}
+			<Layout>
+				{/* <div> */}
 
-        <div className={`${s.main_container}`}>
-          <div className={s.background_container}>
-            <Image src={MainPageBackground} alt={""} className={s.background} />
-            <div className={s.background_gradient}></div>
-          </div>
-          <BannerInfo />
-          <Games />
-          <Total />
-          <CustomBets
-            title="Live bets"
-            isMainPage={true}
-            isGamePage={false}
-            bets={[
-              {
-                time: { date: "25.08.23", time: "17:05" },
-                game_name: "Dice",
-                player: "UserName",
-                wager: 11,
-                multiplier: 3,
-                profit: 5.34,
-                userBg: "#3DBCE5",
-                player_url: "test",
-                trx_url: "test",
-                game_url: "test",
-                network_icon: "test",
-                numBets: 1,
-                gameAddress: "0x563...4ba9",
-              },
-            ]}
-          />
-          <LeaderBoard />
-        </div>
-        {/* </div> */}
-      </Layout>
+				<div className={`${s.main_container}`}>
+					<div className={s.background_container}>
+						<Image src={MainPageBackground} alt={""} className={s.background} />
+						<Image src={MainPageSecondBackground} alt={""} className={s.second_background} />
+						<div className={s.background_gradient}></div>
+					</div>
+					<BannerInfo />
+					<Games />
+					<Total />
+					<CustomBets
+						title="Live bets"
+						isMainPage={true}
+						isGamePage={false}
+						bets={[
+							{
+								time: { date: "25.08.23", time: "17:05" },
+								game_name: "Dice",
+								player: "UserName",
+								wager: 11,
+								multiplier: 3,
+								profit: 5.34,
+								userBg: "#3DBCE5",
+								player_url: "test",
+								trx_url: "test",
+								game_url: "test",
+								network_icon: "test",
+								numBets: 1,
+								gameAddress: "0x563...4ba9",
+							},
+						]}
+					/>
+					<LeaderBoard />
+				</div>
+				{/* </div> */}
+			</Layout>
 
-      {/* <Footer />
+			{/* <Footer />
       <InvitesList />
       <GamesList />
       <ConnectWalletModal /> */}
-    </>
-  );
+		</>
+	);
 }
--- src/widgets/Account/Account.tsx
+++ src/widgets/Account/Account.tsx
@@ -17,28 +17,28 @@ import ExplorerIcon from "@/public/media/account_icons/ExplorerIcon.svg";
 import * as HeaderAccModel from "@/widgets/Account/model";
 import { useUnit } from "effector-react";
 import * as BlurModel from "@/widgets/Blur/model";
-import { useDisconnect } from 'wagmi'
-import {CopyToClipboardButton} from "@/shared/ui/CopyToClipboardButton";
+//import { useDisconnect } from 'wagmi';
+import { CopyToClipboardButton } from "@/shared/ui/CopyToClipboardButton";
 
 export enum Ewallet {
-  Ledger = "Ledger",
-  Coinbase = "Coinbase",
-  Trust_wallet = "Trust Wallet",
-  WalletConnect = "WalletConnect",
+	Ledger = "Ledger",
+	Coinbase = "Coinbase",
+	Trust_wallet = "Trust Wallet",
+	WalletConnect = "WalletConnect",
 }
 
 export interface AccountElementProps {
-  icon: string;
-  name: string;
-  onClick: (() => void) | undefined;
+	icon: string;
+	name: string;
+	onClick: (() => void) | undefined;
 }
 const AccountElement: FC<AccountElementProps> = props => {
-  return (
-    <button className={s.accountElement} onClick={props.onClick}>
-      <Image src={props.icon} alt={""} />
-      <div className={s.text_icon}>{props.name}</div>
-    </button>
-  );
+	return (
+		<button className={s.accountElement} onClick={props.onClick}>
+			<Image src={props.icon} alt={""} />
+			<div className={s.text_icon}>{props.name}</div>
+		</button>
+	);
 };
 
 // export interface WalletProps{ icon: string; wallet: Ewallet; isConnected?: boolean}
@@ -56,61 +56,61 @@ const AccountElement: FC<AccountElementProps> = props => {
 // }
 
 export interface AccountProps {
-  address: string,
-  nickname: string | null
+	address: string,
+	nickname: string | null
 }
 export const Account: FC<AccountProps> = props => {
-  const { disconnect } = useDisconnect();
-  const truncatedAddress = `${props.address.slice(0, 7)}...${props.address.slice(36, 42)}`;
-  // const [copied, setCopied] = useState(false);
+	//const { disconnect } = useDisconnect();
+	const truncatedAddress = `${props.address.slice(0, 7)}...${props.address.slice(36, 42)}`;
+	// const [copied, setCopied] = useState(false);
 
-  const [closeHeaderAccount, setBlur] = useUnit([
-    HeaderAccModel.Close,
-    BlurModel.setBlur,
-  ]);
+	const [closeHeaderAccount, setBlur] = useUnit([
+		HeaderAccModel.Close,
+		BlurModel.setBlur,
+	]);
 
-  const handleHeaderAccClose = () => {
-    setBlur(false);
-    closeHeaderAccount();
-  };
+	const handleHeaderAccClose = () => {
+		setBlur(false);
+		closeHeaderAccount();
+	};
 
-  return (
-    <div className={s.account_container}>
-      <div className={s.account}>
-        <div className={s.main_text}>Account</div>
-        <button className={s.btn_close} onClick={handleHeaderAccClose}>
-          <Image
-            src={Close}
-            alt={""}
-            width={17}
-            height={17}
-            className={s.close_icon}
-          />
-        </button>
-      </div>
-      <div className={s.line}></div>
-      <div className={s.profile}>
-        <Image src={Avatar} alt={""} className={s.avatar_icon} />
-        <div className={s.profile_info}>
-          <div className={s.profile_nickname}>{props.nickname ? props.nickname : truncatedAddress}</div>
-          <div className={s.profile_address}>
-            <div className={s.profile_address}>{truncatedAddress}</div>
-            <div className={s.btn_copy}>
-              <CopyToClipboardButton textToCopy={props.address} />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={s.account_item}>
-        {/* <Wallet wallet={Ewallet.Coinbase} icon={Coinbase} isConnected/> */}
-        {/* <Wallet wallet={Ewallet.Coinbase} icon={Coinbase} /> */}
-        <AccountElement name="Profile" icon={ProfileIcon} onClick={undefined} />
-        <AccountElement name="Explorer" icon={ExplorerIcon} onClick={undefined} />
-        <AccountElement name="Disconnect" icon={ExitIcon} onClick={() => disconnect()} />
-        {/* <AccountElement name="Explorer" icon={Explorer}/>
+	return (
+		<div className={s.account_container}>
+			<div className={s.account}>
+				<div className={s.main_text}>Account</div>
+				<button className={s.btn_close} onClick={handleHeaderAccClose}>
+					<Image
+						src={Close}
+						alt={""}
+						width={17}
+						height={17}
+						className={s.close_icon}
+					/>
+				</button>
+			</div>
+			<div className={s.line}></div>
+			<div className={s.profile}>
+				<Image src={Avatar} alt={""} className={s.avatar_icon} />
+				<div className={s.profile_info}>
+					<div className={s.profile_nickname}>{props.nickname ? props.nickname : truncatedAddress}</div>
+					<div className={s.profile_address}>
+						<div className={s.profile_address}>{truncatedAddress}</div>
+						<div className={s.btn_copy}>
+							<CopyToClipboardButton textToCopy={props.address} />
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className={s.account_item}>
+				{/* <Wallet wallet={Ewallet.Coinbase} icon={Coinbase} isConnected/> */}
+				{/* <Wallet wallet={Ewallet.Coinbase} icon={Coinbase} /> */}
+				<AccountElement name="Profile" icon={ProfileIcon} onClick={undefined} />
+				<AccountElement name="Explorer" icon={ExplorerIcon} onClick={undefined} />
+				{/*<AccountElement name="Disconnect" icon={ExitIcon} onClick={() => disconnect()} />*/}
+				{/* <AccountElement name="Explorer" icon={Explorer}/>
         <AccountElement name="Disconnect" icon={Exit}/> */}
-      </div>
-      <div className={s.ellipse}></div>
-    </div>
-  );
+			</div>
+			<div className={s.ellipse}></div>
+		</div>
+	);
 };
