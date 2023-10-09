582073663a2b2762b201bb05f9274ba57a19d551
Anton@habdevs <hramickih@yandex.ru>
2023-10-10 03:23:40 +0500

    feat: added multipler | pull coinflipAnimation #107

--- src/pages/games/Plinko/index.tsx
+++ src/pages/games/Plinko/index.tsx
@@ -8,18 +8,28 @@ import { LiveBetsWS } from "@/widgets/LiveBets";
 import { WagerModel } from "@/widgets/Wager";
 import { useUnit } from "effector-react";
 import {Plinko} from "@/widgets/Plinko/Plinko";
+import {useAccount} from "wagmi";
+import {CustomWagerRangeInput} from "@/widgets/CustomWagerRangeInput";
+import {WagerGainLoss} from "@/widgets/WagerGainLoss";
+import {ProfitBlock} from "@/widgets/ProfitBlock";
+import {SidePicker} from "@/widgets/CoinFlipSidePicker";
+import s from "@/pages/games/CoinFlip/styles.module.scss";
 
 const WagerContent = () => {
-  const [pressButton] = useUnit([WagerModel.pressButton]);
-  return (
-    <>
-      <WagerInputsBlock />
-      <button className={styles.poker_wager_drawing_cards_btn} onClick={pressButton}>
-        Place a Bet
-      </button>
-      <WagerLowerBtnsBlock game="plinko" />
-    </>
-  );
+    const { isConnected } = useAccount();
+    return (
+        <>
+            <SidePicker />
+            <WagerInputsBlock />
+            <CustomWagerRangeInput inputTitle="Rows" min={8} max={16} />
+            {/*<WagerGainLoss />*/}
+            {/*<ProfitBlock />*/}
+            <button className={s.connect_wallet_btn}>
+                {isConnected ? "Place a bet" : "Connect Wallet"}
+            </button>
+            <WagerLowerBtnsBlock game="plinko" />
+        </>
+    );
 };
 
 export default function PlinkoGame() {
--- src/shared/ui/PlinkoPiramyd/ui/ui.tsx
+++ src/shared/ui/PlinkoPiramyd/ui/ui.tsx
@@ -1,17 +1,22 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import styles from "./ui.module.scss";
 
 interface IPlinkoPyramid {}
 
 export const PlinkoPyramid: FC<IPlinkoPyramid> = () => {
-    const [rowCount, setRowCount] = useState(10);
+    const [rowCount, setRowCount] = useState(8);
+    const [multiplierCount, setMultiplierCount] = useState(9); // Изменяем начальное значение множителей
+
     const generateRows = () => {
         const rows = [];
+        const middleIndex = Math.floor(rowCount / 2);
+
         for (let i = 0; i < rowCount; i++) {
             const dots = [];
             for (let j = 0; j < i + 3; j++) {
                 dots.push(<span className={styles.dot} key={j}></span>);
             }
+
             rows.push(
                 <div className={styles.pyramid_row} key={i}>
                     <span className={styles.ball}>ball</span>
@@ -19,24 +24,41 @@ export const PlinkoPyramid: FC<IPlinkoPyramid> = () => {
                 </div>
             );
         }
+
+        const multipliers = [];
+        for (let i = 0; i < multiplierCount; i++) {
+            const multiplierValue = (middleIndex - Math.abs(middleIndex - i) * 0.5).toFixed(1);
+
+            multipliers.push(
+                <div className={styles.multipiler_cell} key={i}>
+                    {multiplierValue}
+                </div>
+            );
+        }
+
+        rows.push(
+            <div className={styles.pyramid_row} key={rowCount}>
+                <div className={styles.multipiler_container}>{multipliers}</div>
+            </div>
+        );
+
         return rows;
     };
+
     return (
         <div className={styles.container}>
             {generateRows()}
-            <button onClick={() => setRowCount(rowCount + 1)}>Добавить ряд</button>
-            <button onClick={() => setRowCount(rowCount - 1)}>Убрать ряд</button>
+            <button onClick={() => {
+                setRowCount(rowCount + 1);
+                setMultiplierCount(multiplierCount + 1); // Увеличиваем количество множителей при добавлении ряда
+            }}>Добавить ряд</button>
+            <button onClick={() => {
+                if (rowCount > 0) {
+                    setRowCount(rowCount - 1);
+                    setMultiplierCount(multiplierCount - 1); // Убираем ряд и уменьшаем количество множителей
+                }
+            }}>Убрать ряд</button>
         </div>
     );
-
-  //   return (
-  //   <div className={styles.container}>
-  //     <div className={styles.pyramid_row}>
-  //         <span className={styles.ball}>ball</span>
-  //       <div className={styles.dot_container}>
-  //           <span className={styles.dot}></span>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 };
+
