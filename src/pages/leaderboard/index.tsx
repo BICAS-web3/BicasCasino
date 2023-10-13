3ad4c2e5998fc2dea13e94274a251b460b7ad1bb
Rishat2000 <risatosmonov@gmail.com>
2023-10-13 22:22:32 +0600

    added leader board

--- src/pages/leaderboard/index.tsx
+++ src/pages/leaderboard/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { Header } from "@/widgets/header/index";
-import s from "./styles.module.scss";
+import s from "./style.module.scss";
 import { FC, useEffect, useState } from "react";
 import Image, { StaticImageData } from "next/image";
 import CoinFlipColoredIcon from "@/public/media/games_assets/coinflip/icon_colored.svg";
@@ -23,54 +23,13 @@ import tabletBg from "@/public/media/misc/tabletBg.png";
 import phoneBg from "@/public/media/misc/phoneBg.png";
 import { SideBar, SideBarModel } from "@/widgets/SideBar";
 
-import DiceBackground from "@/public/media/games_assets/dice/Background.png";
-import rockPaperScissorsMobileBg from "@/public/media/games_assets/rock_paper_scissors/rockPaperScissorsMobileBg.png";
-import CoinflipBackground from "@/public/media/games_assets/coinflip/Background.png";
-import PokerBackground from "@/public/media/games/poker.png";
-import RPSBackground from "@/public/media/games_assets/rock_paper_scissors/Background.png";
 import { Layout } from "@/widgets/Layout";
+import { Total } from "@/widgets/UserBoard";
+
 import { LeaderBoard } from "@/widgets/LeaderBoard/LeaderBoard";
-import { Total } from "@/widgets/Total";
-
-import pokerMainBg from "@/public/media/games_assets/poker/pokerMainImg.png";
-import pokerLaptopBg from "@/public/media/games_assets/poker/1280Img.png";
-import pokerTabletBg from "@/public/media/games_assets/poker/tabletImg.png";
-import pokerMobileBg from "@/public/media/games_assets/poker/mobileImg.png";
-import pokerClosedSidebarImg from "@/public/media/games_assets/poker/pokerClosedSideBarImg.png";
-
-import coinflipMainBg from "@/public/media/games_assets/coinflip/mainBg.png";
-import coinflipLaptopBg from "@/public/media/games_assets/coinflip/1280Bg.png";
-import coinflipTabletBg from "@/public/media/games_assets/coinflip/tabletBg.png";
-import coinflipMobileBg from "@/public/media/games_assets/coinflip/mobileBg.png";
-import coinflipClosedSidebarImg from "@/public/media/games_assets/coinflip/closedSidebarImg.png";
-
-import diceMainBg from "@/public/media/games_assets/dice/dicePcImg.png";
-import diceLaptopBg from "@/public/media/games_assets/dice/laptopPcImg.png";
-import diceTabletBg from "@/public/media/games_assets/dice/tabletPcImg.png";
-import diceMobileBg from "@/public/media/games_assets/dice/mobileImg.png";
-import diceClosedSidebarImg from "@/public/media/games_assets/dice/closedSideBarImg.png";
-
-import minesMainBg from "@/public/media/games_assets/mines/mainBg.png";
-import minesLaptopBg from "@/public/media/games_assets/mines/1280Bg.png";
-import minesTabletBg from "@/public/media/games_assets/mines/tabletBg.png";
-import minesMobileBg from "@/public/media/games_assets/mines/mobileBg.png";
-import minesClosedSidebarImg from "@/public/media/games_assets/mines/closedSidebarBg.png";
-
-import { Account } from "@/widgets/Account";
-import { GameLayout } from "@/widgets/GameLayout/layout";
-import { GamePage } from "@/widgets/GamePage/GamePage";
-import { CustomBets } from "@/widgets/CustomBets/CustomBets";
-import { AvaibleWallet } from "@/widgets/AvaibleWallet";
 import { useUnit } from "effector-react";
-import { createStore } from "effector";
-import * as BlurModel from "@/widgets/Blur/model";
 import * as SidebarModel from "@/widgets/SideBar/model";
-import { Poker } from "@/widgets/Poker/Poker";
-import PokerGame from "./games/Poker";
-import CoinFlipGame from "./games/CoinFlip";
 import { settingsModel } from "@/entities/settings";
-import { useAccount } from "wagmi";
-import Link from "next/link";
 
 const mobileQuery = "(max-width: 650px)";
 
@@ -188,129 +147,11 @@ const Game: FC<GameProps> = (props) => {
 	);
 };
 
-interface GamesProps { }
-
-const Games: FC<GamesProps> = (props) => {
-	return (
-		<div className={s.games}>
-			{/* <GamesTitle></GamesTitle> */}
-			<div className={s.games_row}>
-				<Game
-					name={"POKER"}
-					description={"Poker"}
-					link={"/games/Poker"}
-					pcImage={pokerMainBg}
-					tabletImage={pokerTabletBg}
-					laptopImage={pokerLaptopBg}
-					mobileImage={pokerMobileBg}
-					closedSidebarImage={pokerClosedSidebarImg}
-				/>
-				<Game
-					name={"DICE"}
-					description={""}
-					link={"/games/Dice"}
-					tabletImage={diceTabletBg}
-					laptopImage={diceLaptopBg}
-					mobileImage={diceMobileBg}
-					pcImage={diceMainBg}
-					closedSidebarImage={diceClosedSidebarImg}
-				/>
-			</div>
-
-			<div className={s.games_row}>
-				<Game
-					name={"COINFLIP"}
-					description={""}
-					link={"/games/CoinFlip"}
-					tabletImage={coinflipTabletBg}
-					laptopImage={coinflipLaptopBg}
-					mobileImage={coinflipMobileBg}
-					pcImage={coinflipMainBg}
-					closedSidebarImage={coinflipClosedSidebarImg}
-				/>
-				<Game
-					name={"MINES"}
-					description={""}
-					link={"/games/Mines"}
-					tabletImage={minesTabletBg}
-					laptopImage={minesLaptopBg}
-					mobileImage={minesMobileBg}
-					pcImage={minesMainBg}
-					closedSidebarImage={minesClosedSidebarImg}
-				/>
-
-				{/* <Game
-                name={'COINFLIP'}
-                description={'COINFLIP GAME very long description that needs to be wrapped to the new line'}
-                link={'/games/CoinFlip'}
-                image_colored={CoinFlipColoredIcon}
-                image_blend={CoinFlipBlendIcon}
-            />  */}
-			</div>
-		</div>
-	);
-};
-
 interface GamesTitleProps { }
 const GamesTitle: FC<GamesTitleProps> = (props) => {
 	return (
 		<div className={s.games_title}>
 			<div>Games</div>
-		</div>
-	);
-};
-
-
-interface BannerInfoProps { }
-const BannerInfo: FC<BannerInfoProps> = (props) => {
-	const [isOpen, isMainWalletOpen, close, open, setBlur] = useUnit([
-		SideBarModel.$isOpen,
-		MainWallet.$isMainWalletOpen,
-		MainWallet.Close,
-		MainWallet.Open,
-		BlurModel.setBlur,
-	]);
-
-	const { isConnected } = useAccount();
-
-	const hideAvaibleWallet = () => {
-		close();
-		setBlur(false);
-		document.documentElement.style.overflow = "visible";
-	};
-
-	const handleConnectWalletBtn = () => {
-		if (!isMainWalletOpen) {
-			open();
-			setBlur(true);
-			document.documentElement.style.overflow = "hidden";
-			window.scrollTo(0, 0);
-		} else {
-			close();
-			setBlur(false);
-			document.documentElement.style.overflow = "visible";
-		}
-	};
-
-	return (
-		<div className={s.banner_info}>
-			<div className={s.header}>Top 1 Casino on the WEB3</div>
-			<div className={s.connect_wallet_container}>
-				{!isConnected && (
-					<>
-						<div className={s.text}>Login via Web3 wallets</div>
-						<div className={s.button} onClick={handleConnectWalletBtn}>
-							Connect Wallet
-						</div>
-					</>
-				)}
-				<div
-					className={`${s.banner_info_avaibleWallet_container} ${!isOpen && s.sidebarClosed
-						} ${isMainWalletOpen && s.walletVisible}`}
-				>
-					<AvaibleWallet hideAvaibleWallet={hideAvaibleWallet} />
-				</div>
-			</div>
 		</div>
 	);
 };
@@ -375,24 +216,14 @@ export default function Home() {
 
 	return (
 		<>
-			<Head>
-				<title>NFT Play | Home page</title>
-			</Head>
-
 			<LiveBetsWS subscription_type={"SubscribeAll"} subscriptions={[]} />
 			<Layout gameName={undefined}>
 				{/* <div> */}
 
 				<div className={`${s.main_container}`}>
-					<div className={s.background_container}>
-						<Image src={currentImage} alt={""} className={s.background} />
-						<div className={s.background_gradient}></div>
-					</div>
-					<BannerInfo />
-					<Games />
 					<Total />
-					<CustomBets
-						title="Live bets"
+					<LeaderBoard
+						title="Leader Board"
 						isMainPage={true}
 						isGamePage={false}
 						game={undefined}
