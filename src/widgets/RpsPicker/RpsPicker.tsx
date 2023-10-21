45ced476b58a6f137e58eec505d650373c7d613a
Anton <you@example.com>
2023-10-21 16:53:48 +0300

    rps pick elements hover - done

--- src/widgets/RpsPicker/RpsPicker.tsx
+++ src/widgets/RpsPicker/RpsPicker.tsx
@@ -2,9 +2,9 @@ import { FC } from "react";
 import s from "./styles.module.scss";
 import Image from "next/image";
 
-import rockImg from "@/public/media/games_assets/rock_paper_scissors/rockIcon.svg";
-import paperImg from "@/public/media/games_assets/rock_paper_scissors/paperIcon.svg";
-import scissorsImg from "@/public/media/games_assets/rock_paper_scissors/scissorsIcon.svg";
+import { RockButton } from "@/shared/SVGs/RockButton";
+import { ScissorsButton } from "@/shared/SVGs/ScissorsButton";
+import { PaperButton } from "@/shared/SVGs/PaperButton";
 
 interface RpsPickerProps {}
 
@@ -12,13 +12,13 @@ export const RpsPicker: FC<RpsPickerProps> = () => {
   return (
     <div className={s.rps_picker_block}>
       <div className={s.rps_picker_block_item}>
-        <Image src={rockImg} alt="rock-icon" />
+        <RockButton />
       </div>
       <div className={s.rps_picker_block_item}>
-        <Image src={scissorsImg} alt="scissors-icon" />
+        <ScissorsButton />
       </div>
       <div className={s.rps_picker_block_item}>
-        <Image src={paperImg} alt="paper-icon" />
+        <PaperButton />
       </div>
     </div>
   );
