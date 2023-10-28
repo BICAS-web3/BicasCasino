23812a336f10f4aab327416ec8239a3566528830
DoctorEenot <dreenot@gmail.com>
2023-10-28 22:27:27 +0300

    wager fix

--- src/widgets/WagerInputsBlock/WagerInputsBlock.tsx
+++ src/widgets/WagerInputsBlock/WagerInputsBlock.tsx
@@ -163,7 +163,7 @@ export const WagerInputsBlock: FC<WagerInputsBlockProps> = ({ }) => {
     }
     const currency = num * exchangeRate;
     console.log(cryptoInputValue, currency, betsAmount);
-    if (true) {
+    if (currency < 5) {
       console.log(currency * betsAmount >= 5);
       setCryptoValue(num);
     } else {
@@ -191,7 +191,7 @@ export const WagerInputsBlock: FC<WagerInputsBlockProps> = ({ }) => {
               console.log("Wager", num);
               const currency = Number((num * exchangeRate).toFixed(7));
               setCurrencyInputValue(currency.toString());
-              if (true) {
+              if (currency < 5) {
                 setCryptoValue(num);
               } else {
                 setCryptoValue(0);
