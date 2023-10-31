5f55ca748661d3426eb12df4b670d3090ac5f7ec
Ilya <ilya3dev@gmail.com>
2023-10-31 11:28:08 +0300

    contract added

--- src/pages/nftmarket/index.tsx
+++ src/pages/nftmarket/index.tsx
@@ -1,28 +1,43 @@
+import { FC, useEffect } from "react";
+import { useAccount, useNetwork, useSwitchNetwork } from "wagmi";
+
 import { Layout } from "@/widgets/Layout";
 import { LiveBetsWS } from "@/widgets/LiveBets";
+import { NFTCard } from "@/widgets/NFTCard";
 
 import s from "./style.module.scss";
-import { NFTCard } from "@/widgets/NFTCard";
+
+const ConnectMarket: FC = () => {
+  const { address } = useAccount();
+
+  const { chain } = useNetwork();
+  const { switchNetwork } = useSwitchNetwork();
+
+  useEffect(() => {
+    if (chain?.id !== 42161 && address) {
+      switchNetwork!(42161);
+    }
+  }, [address]);
+
+  return (
+    <>
+      {" "}
+      {Array.from({ length: 30 }).map((_, i) => (
+        <NFTCard img="" name="NFT name" number={2343} price={34} key={i} />
+      ))}
+    </>
+  );
+};
 
 export default function Home() {
   return (
-    <>
+    <Layout gameName={undefined}>
       <LiveBetsWS subscription_type={"SubscribeAll"} subscriptions={[]} />
-      <Layout gameName={undefined}>
-        <div className={s.main_container}>
-          <div className={s.nft_container}>
-            {Array.from({ length: 30 }).map((_, i) => (
-              <NFTCard
-                img=""
-                name="NFT name"
-                number={2343}
-                price={34}
-                key={i}
-              />
-            ))}
-          </div>
+      <div className={s.main_container}>
+        <div className={s.nft_container}>
+          <ConnectMarket />
         </div>
-      </Layout>
-    </>
+      </div>
+    </Layout>
   );
 }
--- src/widgets/NFTCard/ui/NFTCard.tsx
+++ src/widgets/NFTCard/ui/NFTCard.tsx
@@ -1,8 +1,18 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 import Image from "next/image";
 
+import { ABI as abi } from "@/shared/contracts/MinNFTAbi";
+
 import card_image from "@/public/media/nftMarket_images/nftCard.png";
+
+import {
+  useAccount,
+  useContractRead,
+  useContractWrite,
+  useNetwork,
+  usePrepareContractWrite,
+} from "wagmi";
 
 import s from "./styles.module.scss";
 
@@ -11,13 +21,48 @@ export interface NFTCardProps {
   name: string;
   number: number;
   price: number;
+  id?: number;
 }
 
 export const NFTCard: FC<NFTCardProps> = (props) => {
-  const { name, number, price } = props;
+  const { name, number, price, id } = props;
+
+  const [fee, setFee] = useState<bigint>(BigInt(0));
+
+  const { chain } = useNetwork();
+
+  const { isConnected } = useAccount();
+
+  const { data: mintFee, isSuccess } = useContractRead({
+    chainId: chain?.id,
+    address: `` as `0x${string}`,
+    abi,
+    functionName: "getMintFee",
+    args: [],
+    enabled: true,
+    watch: isConnected,
+  });
+
+  useEffect(() => {
+    if (isSuccess) {
+      setFee(mintFee as bigint);
+    }
+  }, [isSuccess, mintFee]);
+
+  const { config: mintNftConfig } = usePrepareContractWrite({
+    chainId: chain?.id,
+    address: `` as `0x${string}`,
+    abi,
+    functionName: "mintNft",
+    enabled: true,
+    args: [id],
+    value: fee,
+  });
+
+  const { write: mintNft } = useContractWrite(mintNftConfig);
 
   return (
-    <article className={s.nft}>
+    <article onClick={() => mintNft?.()} className={s.nft}>
       <div className={s.nft_image}>
         <Image src={card_image} alt="nft_img" />
       </div>
