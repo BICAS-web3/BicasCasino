f05e5231960ac7113e88e91621e01528fd51f1cb
Ilya <ilya3dev@gmail.com>
2023-11-06 16:39:57 +0300

    close fixes

--- src/shared/tools/index.ts
+++ src/shared/tools/index.ts
@@ -1,5 +1,5 @@
 import { useUnit } from "effector-react";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export function useDebounce<T>(value: T, delay?: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
@@ -35,7 +35,9 @@ export const checkPageClicking = (
 type DeviceType = "main" | "bigTablet" | "laptop" | "tablet" | "phone";
 
 export const useDeviceType = () => {
-  const [deviceType, setDeviceType] = useState<DeviceType | undefined>(undefined);
+  const [deviceType, setDeviceType] = useState<DeviceType | undefined>(
+    undefined
+  );
 
   useEffect(() => {
     const handleResize = () => {
@@ -66,15 +68,48 @@ export const useDeviceType = () => {
   return deviceType;
 };
 
-
-
-export const FB_PIXEL_ID = '1797283080715437';
+export const FB_PIXEL_ID = "1797283080715437";
 
 export const pageview = () => {
-  (window as any).fbq('track', 'PageView')
-}
+  (window as any).fbq("track", "PageView");
+};
 
 // https://developers.facebook.com/docs/facebook-pixel/advanced/
 export const event = (name: any, options = {}) => {
-  (window as any).fbq('track', name, options)
+  (window as any).fbq("track", name, options);
+};
+
+interface useDropdownReturn {
+  isOpen: boolean;
+  toggle: () => void;
+  open: () => void;
+  close: () => void;
+  dropdownRef: React.MutableRefObject<HTMLDivElement | null>;
+}
+
+export function useDropdown(): useDropdownReturn {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef, dropdownRef, setIsOpen]);
+
+  const toggle = useCallback(() => setIsOpen((prev) => !prev), []);
+  const open = useCallback(() => setIsOpen(true), []);
+  const close = useCallback(() => setIsOpen(false), []);
+
+  return { isOpen, toggle, open, close, dropdownRef };
 }
--- src/widgets/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/widgets/LanguageSwitcher/LanguageSwitcher.tsx
@@ -7,6 +7,8 @@ import { LanguageItem } from "@/widgets/LanguageSwitcher/LanguageItem";
 import { FC } from "react";
 import moonIco from "@/public/media/sidebar_icons/moonIco.svg";
 import sunIco from "@/public/media/sidebar_icons/sunIco.svg";
+import { useDropdown } from "@/shared/tools";
+import clsx from "clsx";
 
 export const languages = [
   {
@@ -22,13 +24,15 @@ export const languages = [
 interface LanguageSwitcherProps {}
 
 export const LanguageSwitcher: FC<LanguageSwitcherProps> = (props) => {
+  const { isOpen, dropdownRef, toggle, close } = useDropdown();
   const [activeLanguage, setActiveLanguage] = useState(languages[0]);
-  const [languagesListVisibility, setLanguagesListVisibility] = useState(false);
+  // const [languagesListVisibility, setLanguagesListVisibility] = useState(false);
   const [languagesList, setLanguagesList] = useState(languages);
   const [activeTheme, setActiveTheme] = useState("dark");
 
   const setListVisibility = () => {
-    setLanguagesListVisibility(!languagesListVisibility);
+    // setLanguagesListVisibility(!languagesListVisibility);
+    toggle();
   };
 
   const handleChangeTheme = () => {
@@ -40,7 +44,7 @@ export const LanguageSwitcher: FC<LanguageSwitcherProps> = (props) => {
   }, [activeLanguage]);
 
   return (
-    <div className={s.language_switcher_wrap}>
+    <div ref={dropdownRef} className={s.language_switcher_wrap}>
       <div className={s.language_switcher_title_block}>
         <Image alt="settings-ico" src={settingsIco} width={18} height={18} />
         <h2 className={s.language_switcher_title}>language</h2>
@@ -48,17 +52,19 @@ export const LanguageSwitcher: FC<LanguageSwitcherProps> = (props) => {
       <div className={s.language_switcher_block}>
         <div className={s.language_switcher} onClick={setListVisibility}>
           <h3 className={s.active_language_title}>{activeLanguage.title}</h3>
-          <Image alt="down-ico" src={downIco} width={9} height={5} />
+          <Image
+            className={clsx(s.arr_icon, isOpen && s.arr_icon_open)}
+            alt="down-ico"
+            src={downIco}
+            width={9}
+            height={5}
+          />
         </div>
-        <div
-          className={`${s.languages_list} ${
-            languagesListVisibility && s.visible
-          }`}
-        >
+        <div className={`${s.languages_list} ${isOpen && s.visible}`}>
           {languagesList &&
             languagesList.map((item, _) => (
               <LanguageItem
-                setLanguagesListVisibility={setLanguagesListVisibility}
+                setLanguagesListVisibility={close}
                 setActiveLanguage={setActiveLanguage}
                 {...item}
                 key={item.id}
