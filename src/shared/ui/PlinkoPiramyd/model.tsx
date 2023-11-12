79fa45fb6bf4da2b1a30bdb1f08bbad09cebe708
Anton <you@example.com>
2023-11-12 14:02:16 +0200

    plinko rows - rewritten

--- src/shared/ui/PlinkoPiramyd/model.tsx
+++
@@ -1,9 +0,0 @@
-import { createEffect, createEvent, createStore, sample } from "effector";
-
-// variables
-export const $pickedRows = createStore<number>(0);
-
-// events
-export const pickRows = createEvent<number>();
-
-$pickedRows.on(pickRows, (_, rows) => rows);
