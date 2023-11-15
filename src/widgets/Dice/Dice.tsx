b21730fecfd5f0c4f79d1efc6b814a5054f3737a
Anton <you@example.com>
2023-11-15 13:42:06 +0200

    build error fixed

--- src/widgets/Dice/Dice.tsx
+++ src/widgets/Dice/Dice.tsx
@@ -45,7 +45,7 @@ import { CustomWagerRangeInputModel } from "../CustomWagerRangeInput";
 import s from "./styles.module.scss";
 import clsx from "clsx";
 
-import * as DiceM from '@/pages/games/Dice/model'
+import * as DiceM from "@/pages/games/Dice/model";
 
 enum CoinAction {
   Rotation = "Rotation",
@@ -61,7 +61,7 @@ export interface DiceProps {}
 export const Dice: FC<DiceProps> = () => {
   const { isConnected, address } = useAccount();
   const [
-    setPlayingStatus.
+    setPlayingStatus,
     wagered,
     playSounds,
     switchSounds,
