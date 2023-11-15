e4ec1a5c7bb6b6fbcbd330b5c5febfde0cf4bf50
Anton <you@example.com>
2023-11-15 20:55:51 +0200

    blockies corrections

--- src/widgets/BlockiesAva/BlockiesAva.tsx
+++ src/widgets/BlockiesAva/BlockiesAva.tsx
@@ -6,12 +6,30 @@ interface BlockiesAvaProps {
   address: any;
 }
 
+const colors = [
+  "#9746B5",
+  "#F57731",
+  "#4ED26C",
+  "#E3C549",
+  "#43B581",
+  "#43B581",
+  "#43B581",
+  "#FAA61A",
+  "#F47B68",
+  "#9B84EE",
+  "#44DDBF",
+  "#FF0092",
+  "#3DBCE5",
+  "#FF73FA",
+];
+
 export const BlockiesAva: FC<BlockiesAvaProps> = ({ address }) => {
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
   return (
     <div className={s.blockies_wrap}>
       <Blockies
         seed={`${address.toLowerCase()}`}
-        bgColor="#fff"
         className={s.header_profile_ico_block}
       />
     </div>
