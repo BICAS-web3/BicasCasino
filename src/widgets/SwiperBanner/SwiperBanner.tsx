728c248b7cfc85247ec0e4f07fc3ff5803759900
Anton <you@example.com>
2023-11-24 11:24:38 +0200

    toDraft commit

---
+++ src/widgets/SwiperBanner/SwiperBanner.tsx
@@ -0,0 +1,12 @@
+import {FC} from 'react'
+import s from './styles.module.scss'
+
+interface SwiperBannerProps {}
+
+export const SwiperBanner:FC<SwiperBannerProps> = () => {
+    return (
+        <div className={s.swiper_banner_wrap} >
+            
+        </div>
+    )
+}
