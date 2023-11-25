2e50852918c98584db7b3e9f2309cb2419c9d860
Ilya3dev <ilya3dev@gmail.com>
2023-11-25 23:18:49 +0300

    rps game

--- src/widgets/RockPaperScissors/RockPaperScissors.tsx
+++ src/widgets/RockPaperScissors/RockPaperScissors.tsx
@@ -6,7 +6,40 @@ import { FC, Suspense, useEffect, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useUnit } from "effector-react";
 
-import * as RPSModel from "@/widgets/RockPaperScissors/model";
+import { WagerModel as WagerButtonModel } from "../Wager";
+import * as GameModel from "@/widgets/GamePage/model";
+import { WagerModel } from "../WagerInputsBlock";
+import { CustomWagerRangeInputModel } from "../CustomWagerRangeInput";
+import useSound from "use-sound";
+import {
+  useAccount,
+  useContractEvent,
+  useContractRead,
+  useContractWrite,
+  useNetwork,
+  usePrepareContractWrite,
+} from "wagmi";
+
+import { sessionModel } from "@/entities/session";
+
+import { ABI as RPSABI } from "@/shared/contracts/RPSABI";
+import { ABI as IERC20 } from "@/shared/contracts/ERC20";
+import { useDebounce } from "@/shared/tools";
+import { WagerGainLossModel } from "../WagerGainLoss";
+import { TOKENS } from "@/shared/tokens";
+import { useFeeData } from "wagmi";
+import * as CoinflipM from "./model";
+
+//?-------------------------
+
+export enum ModelType {
+  Paper = "/rps/paperCard.glb",
+  Rock = "/rps/rockCard.glb",
+  Scissors = "/rps/scissorsCard.glb",
+  Quest = "/rps/questCard.glb",
+}
+
+import * as RPSModel from "@/widgets/RpsPicker/model";
 interface ModelProps {
   side: string;
   left: boolean;
@@ -15,6 +48,12 @@ interface ModelProps {
 }
 
 const Model: FC<ModelProps> = ({ side, left, yValue, delay }) => {
+  // const [sideValue, setSideValue] = useState(side);
+
+  // useEffect(() => {
+  //   setSideValue(side);
+  // }, [side]);
+
   const { scene, animations } = useGLTF(side);
   const { actions, mixer } = useAnimations(animations, scene);
   const [is1280, setIs1280] = useState(false);
@@ -94,21 +133,327 @@ const Model: FC<ModelProps> = ({ side, left, yValue, delay }) => {
 
 export const RockPaperScissors = () => {
   const [is1500, setIs1500] = useState(false);
-  const [chosenValue] = useUnit([RPSModel.$isGameValue]);
-  const [value, setValue] = useState("/rps/paperCard.glb");
+  const [value, setValue] = useState<ModelType>(ModelType.Paper);
+
+  //!-------------------------------------------------------------------
+
+  const [
+    setPlayingStatus,
+    playSounds,
+    pickedValue,
+    setActivePicker,
+    pickSide,
+    wagered,
+    setWagered,
+    betsAmount,
+    gameAddress,
+    pickedToken,
+    currentBalance,
+    cryptoValue,
+    stopGain,
+    stopLoss,
+    allowance,
+    setGameStatus,
+    gameStatus,
+    setWonStatus,
+    setLostStatus,
+  ] = useUnit([
+    CoinflipM.setPlayingStatus,
+    GameModel.$playSounds,
+    RPSModel.$pickedValue,
+    RPSModel.setActive,
+    RPSModel.pickValue,
+    WagerButtonModel.$Wagered,
+    WagerButtonModel.setWagered,
+    CustomWagerRangeInputModel.$pickedValue,
+    sessionModel.$gameAddress,
+    WagerModel.$pickedToken,
+    sessionModel.$currentBalance,
+    WagerModel.$cryptoValue,
+    WagerGainLossModel.$stopGain,
+    WagerGainLossModel.$stopLoss,
+    sessionModel.$currentAllowance,
+    GameModel.setGameStatus,
+    GameModel.$gameStatus,
+    GameModel.setWonStatus,
+    GameModel.setLostStatus,
+  ]);
   useEffect(() => {
-    // alert(chosenValue);
-    if (chosenValue === "Paper") {
-      setValue("/rps/paperCard.glb");
-      // alert("paper");
-    } else if (chosenValue === "Rock") {
-      setValue("/rps/rockCard.glb");
-      // alert("rock");
+    // alert(pickedValue);
+    if (pickedValue === RPSModel.RPSValue.Paper) {
+      setValue(ModelType.Paper);
+    } else if (pickedValue === RPSModel.RPSValue.Rock) {
+      setValue(ModelType.Rock);
     } else {
-      setValue("/rps/scissorsCard.glb");
+      setValue(ModelType.Scissors);
       // alert("scissors");
     }
-  }, [chosenValue]);
+  }, [pickedValue]);
+
+  const { chain } = useNetwork();
+  const { address, isConnected } = useAccount();
+  const { data, isError, isLoading } = useFeeData({ watch: true });
+
+  const [waitingResult, setWaitingResult] = useState(false);
+  const [inGame, setInGame] = useState<boolean>(false);
+
+  const [playBackground, { stop: stopBackground }] = useSound(
+    "/static/media/games_assets/music/background2.wav",
+    { volume: 0.1, loop: true }
+  );
+
+  useEffect(() => {
+    console.log("Play sounds", playSounds);
+    if (!playSounds) {
+      stopBackground();
+    } else {
+      playBackground();
+    }
+  }, [playSounds]);
+
+  const { data: GameState, refetch: fetchGameState } = useContractRead({
+    chainId: chain?.id,
+    address: "0x7c47fb44Ce3F14012efB3a1890f5b443424D28b0" as `0x${string}`,
+    abi: RPSABI,
+    functionName: "RockPaperScissors_GetState",
+    args: [address],
+    enabled: true,
+    watch: isConnected,
+  });
+
+  useEffect(() => {
+    if (GameState && !inGame) {
+      if ((GameState as any).ingame) {
+        if (
+          !(GameState as any).isFirstRequest &&
+          (GameState as any).requestID == 0
+        ) {
+          setInGame(true);
+          setActivePicker(false);
+          pickSide((GameState as any).action as number);
+        }
+      } else {
+        setInGame(false);
+      }
+    }
+  }, [GameState]);
+
+  useEffect(() => {
+    inGame ? setPlayingStatus(true) : setPlayingStatus(false);
+  }, [inGame]);
+
+  const { config: allowanceConfig } = usePrepareContractWrite({
+    chainId: chain?.id,
+    address: pickedToken?.contract_address as `0x${string}`,
+    abi: IERC20,
+    functionName: "approve",
+    enabled:
+      pickedToken?.contract_address !=
+      "0x0000000000000000000000000000000000000000",
+    args: [
+      gameAddress,
+      useDebounce(
+        currentBalance
+          ? BigInt(Math.floor(currentBalance * 10000000)) * BigInt(100000000000)
+          : 0
+      ),
+    ],
+  });
+
+  const { write: setAllowance, isSuccess: allowanceIsSet } =
+    useContractWrite(allowanceConfig);
+
+  const [fees, setFees] = useState<bigint>(BigInt(0));
+
+  const { data: VRFFees, refetch: fetchVRFFees } = useContractRead({
+    chainId: chain?.id,
+    address: "0x7c47fb44Ce3F14012efB3a1890f5b443424D28b0" as `0x${string}`,
+    abi: RPSABI,
+    functionName: "getVRFFee",
+    args: [0],
+    watch: true,
+  });
+
+  useEffect(() => {
+    console.log("gas price", data?.gasPrice);
+    if (VRFFees && data?.gasPrice) {
+      setFees(
+        BigInt(VRFFees ? (VRFFees as bigint) : 0) +
+          BigInt(1000000) * (data.gasPrice + data.gasPrice / BigInt(4))
+      );
+    }
+  }, [VRFFees, data]);
+
+  const { config: startPlayingConfig } = usePrepareContractWrite({
+    chainId: chain?.id,
+    address: "0x7c47fb44Ce3F14012efB3a1890f5b443424D28b0" as `0x${string}`,
+    abi: RPSABI,
+    functionName: "RockPaperScissors_Play",
+    args: [
+      useDebounce(
+        BigInt(Math.floor(cryptoValue * 10000000)) * BigInt(100000000000)
+      ),
+      pickedToken?.contract_address,
+      pickedValue,
+      betsAmount,
+      useDebounce(stopGain)
+        ? BigInt(Math.floor((stopGain as number) * 10000000)) *
+          BigInt(100000000000)
+        : BigInt(Math.floor(cryptoValue * 10000000)) *
+          BigInt(100000000000) *
+          BigInt(200),
+      useDebounce(stopLoss)
+        ? BigInt(Math.floor((stopLoss as number) * 10000000)) *
+          BigInt(100000000000)
+        : BigInt(Math.floor(cryptoValue * 10000000)) *
+          BigInt(100000000000) *
+          BigInt(200),
+    ],
+    value:
+      fees +
+      (pickedToken &&
+      pickedToken.contract_address ==
+        "0x0000000000000000000000000000000000000000"
+        ? BigInt(Math.floor(cryptoValue * 10000000) * betsAmount) *
+          BigInt(100000000000)
+        : BigInt(0)),
+    enabled: true,
+  });
+
+  const {
+    write: startPlaying,
+    isSuccess: startedPlaying,
+    error,
+  } = useContractWrite(startPlayingConfig);
+
+  useEffect(() => {
+    if (startedPlaying) {
+      setActivePicker(false);
+      setInGame(true);
+    }
+  }, [startedPlaying]);
+
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
+  useContractEvent({
+    address: "0x7c47fb44Ce3F14012efB3a1890f5b443424D28b0" as `0x${string}`,
+    abi: RPSABI,
+    eventName: "RockPaperScissors_Outcome_Event",
+    listener(log) {
+      if (
+        ((log[0] as any).args.playerAddress as string).toLowerCase() ==
+        address?.toLowerCase()
+      ) {
+        console.log("Found Log!");
+        const wagered =
+          BigInt((log[0] as any).args.wager) *
+          BigInt((log[0] as any).args.numGames);
+        if ((log[0] as any).args.payout > wagered) {
+          console.log("won");
+          const profit = (log[0] as any).args.payout;
+          console.log("profit", profit);
+          const multiplier = Number(profit / wagered);
+          console.log("multiplier", multiplier);
+          //console.log("token", ((log[0] as any).args.tokenAddress as string).toLowerCase());
+          const wagered_token = (
+            (log[0] as any).args.tokenAddress as string
+          ).toLowerCase();
+          const token = TOKENS.find((tk) => tk.address == wagered_token)?.name; //TOKENS[((log[0] as any).args.tokenAddress as string).toLowerCase()];
+          console.log("won token", token);
+          //console.log("available tokens", availableTokens);
+          const profitFloat = Number(profit / BigInt(10000000000000000)) / 100;
+          setWonStatus({
+            profit: profitFloat,
+            multiplier,
+            token: token as string,
+          });
+          setGameStatus(GameModel.GameStatus.Won);
+        } else {
+          console.log("lost");
+          const wageredFloat =
+            Number(wagered / BigInt(10000000000000000)) / 100;
+          console.log("wagered", wageredFloat);
+          setLostStatus(wageredFloat);
+          setGameStatus(GameModel.GameStatus.Lost);
+        }
+        //setShowRedraw(false);
+      }
+    },
+  });
+
+  useEffect(() => {
+    if (wagered) {
+      console.log("Pressed wager");
+      if (inGame) {
+      } else {
+        const total_value = cryptoValue * betsAmount;
+        if (
+          cryptoValue != 0 &&
+          currentBalance &&
+          total_value <= currentBalance
+        ) {
+          if (
+            (!allowance || (allowance && allowance <= cryptoValue)) &&
+            pickedToken?.contract_address !=
+              "0x0000000000000000000000000000000000000000"
+          ) {
+            if (setAllowance) setAllowance();
+          } else {
+            console.log(
+              "Starting playing",
+              startPlaying,
+              BigInt(Math.floor(cryptoValue * 10000000)) * BigInt(100000000000),
+              pickedToken?.contract_address,
+              gameAddress,
+              VRFFees,
+              fees
+            );
+            if (startPlaying) {
+              startPlaying();
+            }
+          }
+        }
+      }
+      setWagered(false);
+    }
+  }, [wagered]);
+
+  useEffect(() => {
+    setActivePicker(true);
+    setInGame(false);
+    if (gameStatus == GameModel.GameStatus.Won) {
+      pickSide(pickedValue);
+    } else if (gameStatus == GameModel.GameStatus.Lost) {
+      pickSide(pickedValue);
+    }
+  }, [gameStatus]);
+
+  const [enemyValue, setEnenmyValue] = useState(ModelType.Quest);
+
+  useEffect(() => {
+    if (gameStatus === GameModel.GameStatus.Draw) {
+      setEnenmyValue(value);
+    } else if (gameStatus === GameModel.GameStatus.Won) {
+      if (pickedValue === RPSModel.RPSValue.Paper) {
+        setEnenmyValue(ModelType.Rock);
+      } else if (pickedValue === RPSModel.RPSValue.Rock) {
+        setEnenmyValue(ModelType.Scissors);
+      } else if (pickedValue === RPSModel.RPSValue.Scissors) {
+        setEnenmyValue(ModelType.Paper);
+      }
+    } else if (gameStatus === GameModel.GameStatus.Lost) {
+      if (pickedValue === RPSModel.RPSValue.Paper) {
+        setEnenmyValue(ModelType.Scissors);
+      } else if (pickedValue === RPSModel.RPSValue.Rock) {
+        setEnenmyValue(ModelType.Paper);
+      } else if (pickedValue === RPSModel.RPSValue.Scissors) {
+        setEnenmyValue(ModelType.Rock);
+      }
+    }
+  }, [gameStatus]);
+  //!-------------------------------------------------------------------
 
   return (
     <div className={s.rps_table_container}>
@@ -150,7 +495,7 @@ export const RockPaperScissors = () => {
               <Model
                 delay={2000}
                 yValue={-0.1}
-                side={"/rps/questCard.glb"}
+                side={enemyValue}
                 left={false}
               />
             </Suspense>
