60f8da059c5d6de4ce2f8cc4ca5f9104a313b1b3
Anton <you@example.com>
2023-12-15 20:11:49 +0200

    build fixed

--- src/shared/ui/ImageLeague/ui/ui.tsx
+++ src/shared/ui/ImageLeague/ui/ui.tsx
@@ -1,13 +1,12 @@
 import React, { FC } from "react";
-import Image from 'next/image'
-import bronzeLeague from '@/public/media/player_icons/bronzeLeague.webp'
-import styles from './ui.module.scss'
+import Image from "next/image";
+import bronzeLeague from "@/public/media/player_icons/bronzeLeague.webp";
+import styles from "./ui.module.scss";
 
 export const ImageLeague: FC<{}> = () => {
   return (
     <div className={styles.image}>
       <div className={styles.image_wrapper}>
-<<<<<<< HEAD
         <Image
           src={bronzeLeague}
           alt={"ImageLeague"}
@@ -15,9 +14,6 @@ export const ImageLeague: FC<{}> = () => {
           height={147}
           className={styles.bronze}
         />
-=======
-        <Image src={bronzeLeague} alt={'ImageLeague'} width={147} height={147} className={styles.bronze} />
->>>>>>> 4251abc14c7beaf25637fc95ae24fbe616614b98
       </div>
       <div className={styles.blur_background}></div>
       <span className={styles.title}>bronze league</span>
