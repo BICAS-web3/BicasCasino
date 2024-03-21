7fc7bd545a248f7d138b5767ceef4f28271bc0df
ilya3dev <ilya3dev@gmail.com>
2024-03-21 13:59:58 +0300

    header-desktop

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-      <section className='space-y-6 pb-8 py-8  md:py-16 lg:py-20 '>
+      {/* <section className='space-y-6 pb-8 py-8  md:py-16 lg:py-20 '>
         <div className='container flex max-w-[64rem] flex-col items-center gap-4 text-center mx-auto'>
           <h1 className='font-bold leading-normal text-3xl sm:text-5xl md:text-6xl lg:text-7xl'>
             Welcome to Next.js 14
@@ -19,7 +19,7 @@ export default function Home() {
 
           <Card />
         </div>
-      </section>
+      </section> */}
     </>
   )
 }
--- components/ui/balance-switcher.tsx
+++ components/ui/balance-switcher.tsx
@@ -1,9 +1,59 @@
-import clsx from 'clsx'
+import { useState } from 'react'
+
 import Image from 'next/image'
 
+import draxTokenIco from '@/public/payment/draxMiniIco.svg'
+import bonusTokenIco from '@/public/payment/bonusCoin.svg'
+
+import clsx from 'clsx'
+
 const BalanceSwitcher = () => {
-  const isDrax = true
-  return <div></div>
+  const [isDrax, setIsDrax] = useState(false)
+
+  const switch_img = [
+    {
+      isDrax: false,
+      src: bonusTokenIco,
+      alt: 'bonus-ico',
+      token: 'bc'
+    },
+    {
+      isDrax: true,
+      src: draxTokenIco,
+      alt: 'drax-ico',
+      token: 'dc'
+    }
+  ]
+
+  return (
+    <div
+      className={clsx(
+        'h-[40px] flex items-center p-[5px] gap-[10px]',
+        'bg-[#121212] border border-[#212121] rounded-[50px]'
+      )}
+    >
+      {switch_img.map(item => (
+        <div
+          onClick={() => setIsDrax(item.isDrax)}
+          className={clsx(
+            'w-full h-full flex items-center pl-2 pr-1 cursor-pointer rounded-[50px]',
+            'text-[#7e7e7e] text-sm font-medium leading-6 text-left uppercase duration-500',
+            isDrax === item.isDrax && 'bg-[#202020] text-white'
+          )}
+        >
+          <Image
+            className='mr-[10px]'
+            src={item.src}
+            width={20}
+            height={20}
+            alt='bonus-ico'
+          />
+          123
+          <span className='text-xs leading-4'>&nbsp;{item.token}</span>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export { BalanceSwitcher }
