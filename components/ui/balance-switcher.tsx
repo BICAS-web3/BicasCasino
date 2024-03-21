19412a196ae01ed0d32d80019d6c9a168a59c4e5
ilya3dev <ilya3dev@gmail.com>
2024-03-21 12:56:14 +0300

    process

--- components/ui/balance-switcher.tsx
+++ components/ui/balance-switcher.tsx
@@ -3,21 +3,7 @@ import Image from 'next/image'
 
 const BalanceSwitcher = () => {
   const isDrax = true
-  return (
-    <div className={s.balance_switcher_wrap}>
-      <div className={s.balance_switcher_block}>
-        <div className={clsx(s.balance_switcher_item, !isDrax && s.active)}>
-          <Image src={bonusTokenIco} width={} height={} alt='bonus-ico' />1
-          <span>&nbsp;bc</span>
-        </div>
-        <div className={clsx(s.balance_switcher_item, isDrax && s.active)}>
-          <Image src={draxTokenIco} width={} height={} alt='bonus-ico' />
-
-          <span>&nbsp;dc</span>
-        </div>
-      </div>
-    </div>
-  )
+  return <div></div>
 }
 
 export { BalanceSwitcher }
