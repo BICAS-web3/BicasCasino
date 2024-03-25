512e5d84ad892b054a98297d0479c2ce57766a94
ilya3dev <ilya3dev@gmail.com>
2024-03-25 13:21:33 +0300

    fix

--- components/custom/mainBanner/components/marquee.tsx
+++ components/custom/mainBanner/components/marquee.tsx
@@ -60,58 +60,62 @@ const data = [
 
 export const MarqueeLine = () => {
   return (
-    <div
-      className={cn(
-        'relative w-screen sm:w-full bg-[#212121] sm:rounded-[99px]',
-        'flex items-center py-[1px] sm:py-[2px] sm:pl-[2px] -ml-4 sm:ml-0'
-      )}
-    >
-      <Marquee
-        autoFill
-        className={cn(
-          'px-5 py-[5px] sm:py-[13px] max-w-[calc(100vw-123px)] sm:max-w-[calc(100%-302px)]',
-          'bg-[#151515] rounded-r-[9px] sm:rounded-[99px] z-[3] relative'
-        )}
-      >
-        {data.map((item, i) => (
-          <div
-            key={item.title}
-            className='flex items-center gap-[5px] mx-[7.5px]'
-          >
-            <span className='text-sm sm:text-base'>#{i + 1}</span>
-            <ArrSVG
-              className={cn(
-                item.grow ? 'text-[#A7F7D1]' : 'text-[#F7BFA7] rotate-180'
-              )}
-            />
-            <Image
-              className='w-5 sm:w-6 h-5 sm:h-6 rounded-full'
-              src={item.img}
-              alt={item.title}
-            />
-            <span className='text-sm sm:text-base'>{item.title}</span>
-          </div>
-        ))}
-      </Marquee>
+    <>
+      {' '}
       <div
         className={cn(
-          'absolute sm:rounded-[99px] bg-[#212121] top-0 right-0 sm:right-[149px] z-[2] text-sm sm:text-base',
-          'flex items-center justify-end gap-[5px] w-[154px] sm:w-[207px] h-full pr-[10px] sm:pr-5'
+          'relative w-screen sm:w-full bg-[#212121] sm:rounded-[99px]',
+          'flex items-center py-[1px] sm:py-[2px] sm:pl-[2px] -ml-4 sm:ml-0'
         )}
       >
-        <ArrSVG className='text-[#A7F7D1] w-[13px] h-[14px]' />
-        <Image src={draxma} alt='draxma' className='w-5 sm:w-6 h-5 sm:h-6' />
-        DraXma
+        <Marquee
+          autoFill
+          className={cn(
+            'px-5 py-[5px] sm:py-[13px] max-w-[calc(100vw-123px)] sm:max-w-[calc(100%-302px)]',
+            'bg-[#151515] rounded-r-[9px] sm:rounded-[99px] z-[3] relative'
+          )}
+        >
+          {data.map((item, i) => (
+            <div
+              key={item.title}
+              className='flex items-center gap-[5px] mx-[7.5px]'
+            >
+              <span className='text-sm sm:text-base'>#{i + 1}</span>
+              <ArrSVG
+                className={cn(
+                  item.grow ? 'text-[#A7F7D1]' : 'text-[#F7BFA7] rotate-180'
+                )}
+              />
+              <Image
+                className='w-5 sm:w-6 h-5 sm:h-6 rounded-full'
+                src={item.img}
+                alt={item.title}
+              />
+              <span className='text-sm sm:text-base'>{item.title}</span>
+            </div>
+          ))}
+        </Marquee>
+        <div
+          className={cn(
+            'absolute sm:rounded-[99px] bg-[#212121] top-0 right-0 sm:right-[149px] z-[2] text-sm sm:text-base',
+            'flex items-center justify-end gap-[5px] w-[154px] sm:w-[207px] h-full pr-[10px] sm:pr-5'
+          )}
+        >
+          <ArrSVG className='text-[#A7F7D1] w-[13px] h-[14px]' />
+          <Image src={draxma} alt='draxma' className='w-5 sm:w-6 h-5 sm:h-6' />
+          DraXma
+        </div>
+        <div
+          className={cn(
+            'absolute h-full rounded-[99px] bg-[#7E15E6] top-0 right-0 w-[213px]',
+            'hidden sm:flex items-center gap-[5px] pr-5 justify-end'
+          )}
+        >
+          <span className='bg-[#F57731] w-2 h-2 rounded-full'></span> Live
+          Trading
+        </div>
       </div>
-      <div
-        className={cn(
-          'absolute h-full rounded-[99px] bg-[#7E15E6] top-0 right-0 w-[213px]',
-          'hidden sm:flex items-center gap-[5px] pr-5 justify-end'
-        )}
-      >
-        <span className='bg-[#F57731] w-2 h-2 rounded-full'></span> Live Trading
-      </div>
-    </div>
+    </>
   )
 }
 
