4f5c8bc4d28256f0d0162cbf91ba7a7c088077d5
ilya3dev <ilya3dev@gmail.com>
2024-04-02 20:03:11 +0300

    process

---
+++ app/games/cars_2/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Games - Cars',
+  description: 'Cars game page '
+}
+
+const Cars = () => {
+  return <div>Cars</div>
+}
+
+export default Cars
