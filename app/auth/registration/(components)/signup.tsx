0246bffd1196b5bc65744e8497954f805f030f93
Ilya3dev <ilya3dev@gmail.com>
2024-04-19 20:46:04 +0300

    option

--- app/auth/registration/(components)/signup.tsx
+++ app/auth/registration/(components)/signup.tsx
@@ -79,7 +79,7 @@ const SignUp: FC<SignupProps> = () => {
     setrtTransition(async () => {
       const { username, password } = values
       form.reset()
-      const data = await fetch(`https:/rew.greekkeepers.io/api/user/register`, {
+      const data = await fetch(`/user/register`, {
         method: 'POST',
         headers: {
           Accept: 'application/json',
