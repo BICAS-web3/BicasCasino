4503868121acd38094a8f07795907b83558dc66c
Anton <you@example.com>
2024-05-12 01:30:58 +0300

    fixes +

--- components/custom/sidebar/components/GamesPopup.tsx
+++ components/custom/sidebar/components/GamesPopup.tsx
@@ -6,6 +6,7 @@ import { usePathname } from 'next/navigation'
 import { LeaderboardSVG } from './icons/games'
 import { useUnit } from 'effector-react'
 import { ModalsModel, SidebarModel } from '@/states'
+import Link from 'next/link';
 
 interface GamesPopupProps {}
 
@@ -17,8 +18,8 @@ export const GamesPopup:FC<GamesPopupProps> = ({}) => {
         <div className={`fixed ${open ? 'left-0 sm:left-[257px]' : 'left-0 sm:left-[90px]'} ${isGames ? "scale-[1] origin-left opacity-1 visible" : "scale-[0.6] opacity-0 invisible"} transition-all duration-200 flex flex-col justify-between z-[99999] h-[calc(100%_-_112px)] sm:h-[calc(100%_-_60px)] top-[56px] sm:top-[60px] bg-[#121212] w-[225px] p-[0_20px] box-bordeer`}>
           <div>
             {SGames.items.map((item, index) => (
-                <div
-                // href={`/games/${stringRemoveSpacing(item.title)}`}
+                <Link
+                href={`/games/${stringRemoveSpacing(item.title)}`}
                 className={cn(
                     'w-full flex gap-3 cursor-pointer justify-start items-center flex-nowrap bg-[#121212] relative min-h-12 overflow-hidden rounded-xl',
                     `/${stringRemoveSpacing(item.title)}` === params
@@ -26,12 +27,27 @@ export const GamesPopup:FC<GamesPopupProps> = ({}) => {
                     : 'text-[#979797] hover:text-white'
                 )}
                 key={`sidebar-games-${stringRemoveSpacing(item.title)}-${index}`}
+                onClick={() => {
+
+                 if(window.innerHeight < 650) {
+                  const element = document.documentElement
+                  if (element.requestFullscreen) {
+                    element.requestFullscreen()
+                  } else if ((element as any).webkitRequestFullscreen) {
+                    /* Safari */
+                    ;(element as any).webkitRequestFullscreen()
+                  } else if ((element as any).msRequestFullscreen) {
+                    /* IE11 */
+                    ;(element as any).msRequestFullscreen()
+                  }
+                 }
+                }}
                 >
-                <span>{item.icon}</span>
-                <span className='leading-5 text-base font-bold tracking-wide w-full max-w-36 text-left truncate'>
-                    {item.title}
-                </span>
-                </div>
+                  <span>{item.icon}</span>
+                  <span className='leading-5 text-base font-bold tracking-wide w-full max-w-36 text-left truncate'>
+                      {item.title}
+                  </span>
+                </Link>
             ))}
           </div>
           <div className='flex h-[72px] items-center'>
