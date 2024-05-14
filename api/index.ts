da4dd56949de2be0d72958f85778002e71aa61ae
ewrff <ewrf>
2024-05-15 01:26:34 +0300

    fixes

--- api/index.ts
+++ api/index.ts
@@ -2,8 +2,8 @@ import { UserType } from '@/states/user_model.store'
 import { createEffect, createEvent } from 'effector'
 import { JWT } from 'next-auth/jwt'
 
-export const BaseApiUrl = 'https://rew.greekkeepers.io/api'
-export const BaseStaticUrl = 'https://rew.greekkeepers.io/static'
+export const BaseApiUrl = '/api'
+export const BaseStaticUrl = '/static'
 export const P2WayUrl = 'https://stage.p2way.fyi'
 
 export type T_ErrorText = {
