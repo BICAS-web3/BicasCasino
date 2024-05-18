be812cf3ace691f406dd9b5e700b4b23a992f728
ewrff <ewrf>
2024-05-18 18:14:23 +0300

    process

--- components/custom/sidebar/sidebar.tsx
+++ components/custom/sidebar/sidebar.tsx
@@ -56,10 +56,10 @@ const Sidebar = () => {
         <div
           id='sidebar'
           className={cn(
-            `bg-[#181818]  sm:sticky z-[55] sm:z-50 left-0 bottom-0 sm:top-[59px] flex flex-col w-full border-r-[1px] border-[#252525]`,
+            `bg-[#181818]  sm:sticky z-[55] sm:z-50 left-0 sm:top-[59px] flex flex-col w-full border-r-[1px] border-[#252525]`,
             open
-              ? 'sm:w-[257px] h-[calc(100vh_-_56px)] sm:h-[calc(100vh_-_60px)]'
-              : 'sm:w-[90px] h-max sm:h-[calc(100vh_-_60px)]',
+              ? 'sm:w-[257px] h-[calc(100vh_-_56px)] sm:h-[calc(100vh_-_60px)] top-[56px]'
+              : 'sm:w-[90px] h-max sm:h-[calc(100vh_-_60px)] bottom-0',
             open || !isGamePage ? 'fixed' : 'relative'
           )}
         >
